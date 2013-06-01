8b25bfd46ee14ab9025649973d674e2a4a692433
Robert Konrad <robert@ktx-software.com>
2013-06-01 04:42:02 +0200

    Made Android audio work.

--- Backends/Android/Java-Sources/com/ktxsoftware/kore/KoreActivity.java
+++ Backends/Android/Java-Sources/com/ktxsoftware/kore/KoreActivity.java
@@ -8,10 +8,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class KoreActivity extends Activity implements AudioTrack.OnPlaybackPositionUpdateListener {
-	private AudioTrack audio;
-	private byte[] audioBuffer;
-	
+public class KoreActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle state) {
 		super.onCreate(state);
@@ -19,21 +16,20 @@ public class KoreActivity extends Activity implements AudioTrack.OnPlaybackPosit
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(new KoreView(this));
 		int bufferSize = AudioTrack.getMinBufferSize(44100,  AudioFormat.CHANNEL_OUT_STEREO,  AudioFormat.ENCODING_PCM_16BIT);
-		audioBuffer = new byte[bufferSize / 3];
-		audio = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
-		audio.setPlaybackPositionUpdateListener(this);
-		audio.setPositionNotificationPeriod(bufferSize / 4 / 3);
+
+		final AudioTrack audio = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
 		audio.play();
-	}
 
-	@Override
-	public void onMarkerReached(AudioTrack track) {
-		
-	}
-
-	@Override
-	public void onPeriodicNotification(AudioTrack track) {
-		KoreLib.writeAudio(audioBuffer, audioBuffer.length);
-		track.write(audioBuffer, 0, audioBuffer.length);
+		Runnable audioThread = new Runnable() {
+			public void run() {
+				Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+				byte[] audioBuffer = new byte[1000];
+				for (;;) {
+					KoreLib.writeAudio(audioBuffer, audioBuffer.length);
+					audio.write(audioBuffer, 0, audioBuffer.length);
+				}
+			}
+		};
+		new Thread(audioThread).start();
 	}
 }
