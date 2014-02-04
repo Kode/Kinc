faa205196988c5b474ada96bb90d75881a83211d
Robert Konrad <robert@ktx-software.com>
2014-02-05 00:35:09 +0100

    Fixed OpenGL render target size.

--- Backends/OpenGL2/Sources/Kore/RenderTargetImpl.cpp
+++ Backends/OpenGL2/Sources/Kore/RenderTargetImpl.cpp
@@ -5,7 +5,23 @@
 
 using namespace Kore;
 
+namespace {
+	int pow(int pow) {
+		int ret = 1;
+		for (int i = 0; i < pow; ++i) ret *= 2;
+		return ret;
+	}
+	
+	int getPower2(int i) {
+		for (int power = 0; ; ++power)
+			if (pow(power) >= i) return pow(power);
+	}
+}
+
 RenderTarget::RenderTarget(int width, int height, bool zBuffer, bool antialiasing, RenderTargetFormat format) : width(width), height(height) {
+	texWidth = getPower2(width);
+	texHeight = getPower2(height);
+	
 	glGenTextures(1, &_texture);
 	glBindTexture(GL_TEXTURE_2D, _texture);
 	
@@ -16,7 +32,7 @@ RenderTarget::RenderTarget(int width, int height, bool zBuffer, bool antialiasin
 
 	glGenFramebuffers(1, &_framebuffer);
 	glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture, 0);
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
