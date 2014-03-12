8b2f0d4cb65f0eeb7b2371715ca3d6fff525784b
Robert Konrad <robert@ktx-software.com>
2014-03-12 10:35:57 +0100

    Don't adjust mouse coordinates to screen resolution differences.

--- Sources/Kore/Input/Mouse.cpp
+++ Sources/Kore/Input/Mouse.cpp
@@ -7,11 +7,6 @@ using namespace Kore;
 
 namespace {
 	Mouse mouse;
-	
-	void adjust(MouseEvent& event) {
-		event.multiply(static_cast<float>(Kore::Application::the()->width()) / static_cast<float>(Kore::System::screenWidth()),
-					   static_cast<float>(Kore::Application::the()->height()) / static_cast<float>(Kore::System::screenHeight()));
-	}
 }
 
 Mouse* Mouse::the() {
@@ -20,35 +15,30 @@ Mouse* Mouse::the() {
 
 void Mouse::_move(MouseEvent event) {
 	if (Move != nullptr) {
-		adjust(event);
 		Move(event);
 	}
 }
 
 void Mouse::_pressLeft(MouseEvent event) {
 	if (PressLeft != nullptr) {
-		adjust(event);
 		PressLeft(event);
 	}
 }
 
 void Mouse::_pressRight(MouseEvent event) {
 	if (PressRight != nullptr) {
-		adjust(event);
 		PressRight(event);
 	}
 }
 
 void Mouse::_releaseLeft(MouseEvent event) {
 	if (ReleaseLeft != nullptr) {
-		adjust(event);
 		ReleaseLeft(event);
 	}
 }
 
 void Mouse::_releaseRight(MouseEvent event) {
 	if (ReleaseRight != nullptr) {
-		adjust(event);
 		ReleaseRight(event);
 	}
 }
