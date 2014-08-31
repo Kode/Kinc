b014130a8c27ff253c9f3d16aed2b11fc8dc60ff
Robert Konrad <robert@ktx-software.com>
2014-08-31 04:04:36 +0200

    Remove directories properly.

--- Files.js
+++ Files.js
@@ -1,23 +1,23 @@
 ﻿var fs = require('fs');
-var pathSep = require('path').sep;
+var pa = require('path');
 
 exports.exists = function (path) {
 	return fs.existsSync(path.path);
 };
 
 exports.createDirectories = function (path) {
-	var dirs = path.path.split(pathSep);
+	var dirs = path.path.split(pa.sep);
 	var root = "";
 
 	while (dirs.length > 0) {
 		var dir = dirs.shift();
 		if (dir === "") { // If directory starts with a /, the first path will be an empty string.
-			root = pathSep;
+			root = pa.sep;
 		}
 		if (!fs.existsSync(root + dir)) {
 			fs.mkdirSync(root + dir);
 		}
-		root += dir + pathSep;
+		root += dir + pa.sep;
 	}
 };
 
@@ -35,6 +35,24 @@ exports.newDirectoryStream = function (path) {
 	return fs.readdirSync(path.path);
 };
 
+var rmdir = function (dir) {
+	var list = fs.readdirSync(dir);
+	for (var i = 0; i < list.length; ++i) {
+		var filename = pa.join(dir, list[i]);
+		var stat = fs.statSync(filename);
+		if (filename == "." || filename == "..") {
+
+		}
+		else if (stat.isDirectory()) {
+			rmdir(filename);
+		}
+		else {
+			fs.unlinkSync(filename);
+		}
+	}
+	fs.rmdirSync(dir);
+};
+
 exports.removeDirectory = function (path) {
-	if (fs.existsSync(path.path)) fs.rmdirSync(path.path);
+	if (fs.existsSync(path.path)) rmdir(path.path);
 };
