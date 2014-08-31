2f705a67f313114279c8254b31b046b709c0c4c8
Robert Konrad <robert@ktx-software.com>
2014-08-31 07:13:01 +0200

    Take parameters from options object.

--- koremake.js
+++ koremake.js
@@ -1,4 +1,45 @@
-require('./main.js').run({
+var args = process.argv;
+
+var from = '.';
+var to = 'build';
+
+if (os.platform() === "linux") {
+	var platform = Platform.Linux;
+}
+else if (os.platform() === "win32") {
+	var platform = Platform.Windows;
+}
+else {
+	var platform = Platform.OSX;
+}
+
+for (var i = 2; i < args.length; ++i) {
+	var arg = args[i];
+	
+	if (arg == "pch") Options.setPrecompiledHeaders(true);
+	else if (arg.startsWith("intermediate=")) Options.setIntermediateDrive(arg.substr(13));
+	else if (arg.startsWith("gfx=")) Options.setGraphicsApi(arg.substr(4));
+	else if (arg.startsWith("vs=")) Options.setVisualStudioVersion(arg.substr(3));
+
+	else if (arg.startsWith("from=")) from = arg.substr(5);
+	else if (arg.startsWith("to=")) to = arg.substr(3);
+
+	else {
+		for (p in Platform) {
+			if (arg === Platform[p]) {
+				platform = Platform[p];
+			}
+		}
+	}
+}
+
+require('./main.js').run(
+{
+	from: from,
+	to: to,
+	platform: platform
+},
+{
 	info: console.log,
 	error: console.log
-},, function () { });
+}, function () { });
--- main.js
+++ main.js
@@ -179,43 +179,10 @@ function exportProject(from, to, platform) {
 	}
 }
 
-exports.run = function (loglog, callback) {
+exports.run = function (options, loglog, callback) {
 	log.set(loglog);
 
-	var args = process.argv;
-
-	var from = '.';
-	var to = 'build';
-
-	if (os.platform() === "linux") {
-		var platform = Platform.Linux;
-	}
-	else if (os.platform() === "win32") {
-		var platform = Platform.Windows;
-	}
-	else {
-		var platform = Platform.OSX;
-	}
-
-	for (var i = 2; i < args.length; ++i) {
-		var arg = args[i];
-
-		if (arg == "pch") Options.setPrecompiledHeaders(true);
-		else if (arg.startsWith("intermediate=")) Options.setIntermediateDrive(arg.substr(13));
-		else if (arg.startsWith("gfx=")) Options.setGraphicsApi(arg.substr(4));
-		else if (arg.startsWith("vs=")) Options.setVisualStudioVersion(arg.substr(3));
-
-		else if (arg.startsWith("from=")) from = arg.substr(5);
-		else if (arg.startsWith("to=")) to = arg.substr(3);
-
-		else {
-			for (p in Platform) {
-				if (arg === Platform[p]) {
-					platform = Platform[p];
-				}
-			}
-		}
-	}
-	exportProject(Paths.get(from), Paths.get(to), platform);
+	
+	exportProject(Paths.get(options.from), Paths.get(options.to), options.platform);
 	callback();
 };
