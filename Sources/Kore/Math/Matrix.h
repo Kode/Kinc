fcf83a16639ad707c7ce8f221d5dc0f876d83709
Robert Konrad <robert@ktx-software.com>
2014-11-23 16:09:57 +0100

    Added lookAlong function to Matrix.

--- Sources/Kore/Math/Matrix.h
+++ Sources/Kore/Math/Matrix.h
@@ -70,6 +70,7 @@ namespace Kore {
 			m.Set(3, 2, -1);
 			return m;
 		}
+
 		// fov in deg
 		static myType Perspective(float fov, float aspect, float near, float far) {
 			myType m;
@@ -85,6 +86,21 @@ namespace Kore {
 
 		static myType lookAt(vec3 eye, vec3 at, vec3 up) {
 			vec3 zaxis = at - eye;
+			zaxis.normalize();
+			vec3 xaxis = up % zaxis;
+			xaxis.normalize();
+			vec3 yaxis = zaxis % xaxis;
+
+			mat4 view;
+			view.Set(0, 0, xaxis.x()); view.Set(0, 1, xaxis.y()); view.Set(0, 2, xaxis.z()); view.Set(0, 3, -xaxis.dot(eye));
+			view.Set(1, 0, yaxis.x()); view.Set(1, 1, yaxis.y()); view.Set(1, 2, yaxis.z()); view.Set(1, 3, -yaxis.dot(eye));
+			view.Set(2, 0, zaxis.x()); view.Set(2, 1, zaxis.y()); view.Set(2, 2, zaxis.z()); view.Set(2, 3, -zaxis.dot(eye));
+			view.Set(3, 0, 0);         view.Set(3, 1, 0);         view.Set(3, 2, 0);         view.Set(3, 3, 1);
+			return view;
+		}
+
+		static myType lookAlong(vec3 axis, vec3 eye, vec3 up) {
+			vec3 zaxis = axis;
 			zaxis.normalize();
 			vec3 xaxis = up % zaxis;
 			xaxis.normalize();
