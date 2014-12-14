3f88f3fad0b0c8b904f82013ac43b0a53049c3a2
Robert Konrad <robert@ktx-software.com>
2014-12-14 21:54:26 +0100

    Added video stream support to audio mixer.

--- Sources/Kore/Audio/Mixer.h
+++ Sources/Kore/Audio/Mixer.h
@@ -4,10 +4,18 @@
 #include "SoundStream.h"
 
 namespace Kore {
+#ifdef KOREVIDEO
+	class VideoSoundStream;
+#endif
+
 	namespace Mixer {
 		void init();
 		void play(Sound* sound);
 		void play(SoundStream* stream);
 		void stop(SoundStream* stream);
+#ifdef KOREVIDEO
+		void play(VideoSoundStream* stream);
+		void stop(VideoSoundStream* stream);
+#endif
 	}
 }
