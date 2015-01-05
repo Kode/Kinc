9abb2c505867c1e0b69911d2cb02c2a271202efc
Robert Konrad <robert@ktx-software.com>
2015-01-05 13:23:25 +0100

    Fix empty paths.

--- Paths.js
+++ Paths.js
@@ -1,7 +1,8 @@
 ﻿var path = require('path');
 var Path = require('./Path.js');
 
-exports.get = function (a) {
+exports.get = function () {
+	if (arguments.length == 1 && arguments[0] === '') return new Path('');
 	var pathstrings = [];
 	for (var i = 0; i < arguments.length; ++i) {
 		if (typeof (arguments[i]) === 'string') pathstrings.push(arguments[i]);
