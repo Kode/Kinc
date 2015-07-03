7d1c95cd8929422c3242d841e28d7a99fc50ace1
Robert Konrad <robert@ktx-software.com>
2015-07-04 00:08:25 +0200

    Fixed Windows backend compilation.

--- Backends/Windows/Sources/Kore/Vr/VrInterface.h
+++ Backends/Windows/Sources/Kore/Vr/VrInterface.h
@@ -1,26 +1,16 @@
 #pragma once
 
-
+/*
 #include <kha/vr/SensorState.h>
 #include <kha/vr/TimeWarpParms.h>
-
 #include <kha/Image.h>
-
 #include <kha/math/Quaternion.h>
 
-
-
-
-
 namespace Kore {
-
 	namespace VrInterface {
-
 		kha::vr::SensorState_obj* GetSensorState();
-
 		void WarpSwap(kha::vr::TimeWarpParms_obj* parms);
-
 		void* Init(void* hinst);
-		
 	}
 }
+*/
