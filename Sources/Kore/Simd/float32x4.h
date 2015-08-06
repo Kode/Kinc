427b98de738a307e1a3230b60e16f29e3db5badb
Robert Konrad <robert@ktx-software.com>
2015-08-06 15:58:50 +0200

    Added initial simd support.

---
+++ Sources/Kore/Simd/float32x4.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <xmmintrin.h>
+
+namespace Kore {
+	typedef __m128 float32x4;
+
+	float32x4 load(float a, float b, float c, float d) {
+		float values[] = { a, b, c, d };
+		return _mm_load_ps1(&values[0]);
+	}
+
+	float get(float32x4 t, int index) {
+		return t.m128_f32[index];
+	}
+
+	float32x4 abs(float32x4 t) {
+		__m128 mask = _mm_set_ps1(-0.f);
+		return _mm_andnot_ps(mask, t);
+	}
+
+	float32x4 add(float32x4 a, float32x4 b) {
+		return _mm_add_ps(a, b);
+	}
+
+	float32x4 div(float32x4 a, float32x4 b) {
+		return _mm_div_ps(a, b);
+	}
+	
+	float32x4 mul(float32x4 a, float32x4 b) {
+		return _mm_mul_ps(a, b);
+	}
+
+	float32x4 neg(float32x4 t) {
+		__m128 negative = _mm_set_ps1(-1.0f);
+		return _mm_mul_ps(t, negative);
+	}
+	
+	float32x4 reciprocalApproximation(float32x4 t) {
+		return _mm_rcp_ps(t);
+	}
+
+	float32x4 reciprocalSqrtApproximation(float32x4 t) {
+		return _mm_rsqrt_ps(t);
+	}
+
+	float32x4 sub(float32x4 a, float32x4 b) {
+		return _mm_sub_ps(a, b);
+	}
+
+	float32x4 sqrt(float32x4 t) {
+		return _mm_sqrt_ps(t);
+	}
+}
