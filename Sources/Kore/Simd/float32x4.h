713c366004f658ccd1a95002b67249fc01f0246a
Robert Konrad <robert@ktx-software.com>
2015-08-07 01:56:30 +0200

    Implemented scalar fallback for simd code.

--- Sources/Kore/Simd/float32x4.h
+++ Sources/Kore/Simd/float32x4.h
@@ -2,6 +2,8 @@
 
 #include <xmmintrin.h>
 
+#if defined(__SSE__) || _M_IX86_FP == 2 || _M_IX86_FP == 1
+
 namespace Kore {
 	typedef __m128 float32x4;
 
@@ -60,3 +62,118 @@ namespace Kore {
 		return _mm_sqrt_ps(t);
 	}
 }
+
+#else
+
+#include <Kore/Math/Core.h>
+
+namespace Kore {
+	struct float32x4 {
+		float values[4];
+	};
+
+	inline float32x4 load(float a, float b, float c, float d) {
+		float32x4 value;
+		value.values[0] = a;
+		value.values[1] = b;
+		value.values[2] = c;
+		value.values[3] = d;
+		return value;
+	}
+
+	inline float32x4 loadAll(float t) {
+		float32x4 value;
+		value.values[0] = t;
+		value.values[1] = t;
+		value.values[2] = t;
+		value.values[3] = t;
+		return value;
+	}
+
+	inline float get(float32x4 t, int index) {
+		return t.values[index];
+	}
+
+	inline float32x4 abs(float32x4 t) {
+		float32x4 value;
+		value.values[0] = Kore::abs(t.values[0]);
+		value.values[1] = Kore::abs(t.values[1]);
+		value.values[2] = Kore::abs(t.values[2]);
+		value.values[3] = Kore::abs(t.values[3]);
+		return value;
+	}
+
+	inline float32x4 add(float32x4 a, float32x4 b) {
+		float32x4 value;
+		value.values[0] = a.values[0] + b.values[0];
+		value.values[1] = a.values[1] + b.values[1];
+		value.values[2] = a.values[2] + b.values[2];
+		value.values[3] = a.values[3] + b.values[3];
+		return value;
+	}
+
+	inline float32x4 div(float32x4 a, float32x4 b) {
+		float32x4 value;
+		value.values[0] = a.values[0] / b.values[0];
+		value.values[1] = a.values[1] / b.values[1];
+		value.values[2] = a.values[2] / b.values[2];
+		value.values[3] = a.values[3] / b.values[3];
+		return value;
+	}
+
+	inline float32x4 mul(float32x4 a, float32x4 b) {
+		float32x4 value;
+		value.values[0] = a.values[0] * b.values[0];
+		value.values[1] = a.values[1] * b.values[1];
+		value.values[2] = a.values[2] * b.values[2];
+		value.values[3] = a.values[3] * b.values[3];
+		return value;
+	}
+
+	inline float32x4 neg(float32x4 t) {
+		float32x4 value;
+		value.values[0] = -t.values[0];
+		value.values[1] = -t.values[1];
+		value.values[2] = -t.values[2];
+		value.values[3] = -t.values[3];
+		return value;
+	}
+
+	inline float32x4 reciprocalApproximation(float32x4 t) {
+		float32x4 value;
+		value.values[0] = 0;
+		value.values[1] = 0;
+		value.values[2] = 0;
+		value.values[3] = 0;
+		return value;
+	}
+
+	inline float32x4 reciprocalSqrtApproximation(float32x4 t) {
+		float32x4 value;
+		value.values[0] = 0;
+		value.values[1] = 0;
+		value.values[2] = 0;
+		value.values[3] = 0;
+		return value;
+	}
+
+	inline float32x4 sub(float32x4 a, float32x4 b) {
+		float32x4 value;
+		value.values[0] = a.values[0] - b.values[0];
+		value.values[1] = a.values[1] - b.values[1];
+		value.values[2] = a.values[2] - b.values[2];
+		value.values[3] = a.values[3] - b.values[3];
+		return value;
+	}
+
+	inline float32x4 sqrt(float32x4 t) {
+		float32x4 value;
+		value.values[0] = Kore::sqrt(t.values[0]);
+		value.values[1] = Kore::sqrt(t.values[1]);
+		value.values[2] = Kore::sqrt(t.values[2]);
+		value.values[3] = Kore::sqrt(t.values[3]);
+		return value;
+	}
+}
+
+#endif
