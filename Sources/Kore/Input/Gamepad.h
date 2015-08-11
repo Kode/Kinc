f35b6551359aaa429f169a42e208fb8972835e5d
ratalaika <ratalaika@gmail.com>
2015-08-11 21:08:46 +0200

    Improved gamepads

--- Sources/Kore/Input/Gamepad.h
+++ Sources/Kore/Input/Gamepad.h
@@ -1,6 +1,15 @@
 #pragma once
 
 namespace Kore {
+	// Information about the gamepad
+	class GamepadInfo {
+	public:
+		GamepadInfo() : devId(-1), vendor(nullptr), producName(nullptr) {};
+		int devId;
+		wchar_t* vendor;
+		wchar_t* producName;
+	};
+
 	class Gamepad {
 	public:
 		static Gamepad* get(int num);
@@ -10,5 +19,7 @@ namespace Kore {
 		//called by backend
 		void _axis(int axis, float value);
 		void _button(int button, float value);
+
+		GamepadInfo info;
 	};
 }
