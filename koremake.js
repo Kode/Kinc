334311929bf70738bb5f2d91ac217a39aeb407a7
ratalaika <ratalaika@gmail.com>
2015-08-13 17:41:55 +0200

    Updated parameters, added --update parameter, and update html5 compilation

--- koremake.js
+++ koremake.js
@@ -30,59 +30,241 @@ if (!String.prototype.endsWith) {
 	});
 }
 
-var args = process.argv;
 
-var from = '.';
-var to = 'build';
-var gfx = GraphicsApi.Direct3D9;
-var vs = VisualStudioVersion.VS2015;
-var compile = false;
-var run = false;
-var nokrafix = false;
-
+var defaultTarget;
 if (os.platform() === "linux") {
-	var platform = Platform.Linux;
+	defaultTarget = Platform.Linux;
 }
 else if (os.platform() === "win32") {
-	var platform = Platform.Windows;
+	defaultTarget = Platform.Windows;
 }
 else {
-	var platform = Platform.OSX;
+	defaultTarget = Platform.OSX;
 }
+
+var options = [
+	{
+		full: 'from',
+		value: true,
+		description: 'Location of your project',
+		default: '.'
+
+	},
+	{
+		full: 'to',
+		value: true,
+		description: 'Build location',
+		default: 'build'
+	},
+	{
+		full: 'emcc',
+		value: true,
+		description: 'Location of emscripten shader compiler',
+	},
+	{
+		full: 'pch',
+		description: 'Use precompiled headers for C++ targets',
+		value: false
+	},
+	{
+		full: 'intermediate',
+		description: 'Intermediate location for object files.',
+		value: true,
+		default: '',
+		hidden: true
+	},
+	{
+		full: 'visualstudio',
+		short: 'vs',
+		description: 'Version of Visual Studio to use',
+		value: true,
+		default: VisualStudioVersion.VS2015
+	},
+	{
+		full: 'nokrafix',
+		description: 'Switch off the new shader compiler',
+		value: false
+	},
+	{
+		full: 'compile',
+		description: 'Compile executable',
+		value: false
+	},
+	{
+		full: 'run',
+		description: 'Run executable',
+		value: false
+	},
+	{
+		full: 'update',
+		description: 'Update Kore and it\'s submodules',
+		value: false
+	},
+	{
+		full: 'platform',
+		short: 'p',
+		value: true,
+		description: 'Target platform',
+		default: defaultTarget,
+		values: Platform
+	},
+	{
+		full: 'graphicsApi',
+		short: 'gfx',
+		description: 'Graphics api to use',
+		value: true,
+		default: GraphicsApi.Direct3D9,
+		values: GraphicsApi
+	},
+];
+
+var parsedOptions = {
+
+};
+
+function printHelp() {
+	console.log("Usage: ");
+	console.log("node Kore/make.js <platform> [args]");
+	console.log("")
+	console.log("The possible options are:")
+
+	for (var o in options) {
+		var option = options[o];
+		if (option.hidden) {
+			continue;
+		}
+
+		var str = '';
+		if (option.short) {
+			str += '-' + option.short + ' '
+		}
+		str += '--' + option.full;
+
+		// Add tabs to allow show in a fancy way
+		if(str.length <= 6) {
+			str += '\t\t\t';
+		} else
+		if(str.length <= 15) {
+			str += '\t\t';
+		} else {
+			str += '\t';
+		}
+
+		str += option.description
+
+		// Add the possible values
+		if(option.default) {
+			str += ", default '";
+			str += option.default;
+			str += "'";
+		}
+
+		// Add the possible values
+		if(option.values) {
+			str += ", options: \n";
+			for (v in option.values) {
+				str += " * " + option.values[v] + "\n";
+			}
+		}
+
+		console.log(str);
+	}
+}
+
+for (var o in options) {
+	var option = options[o];
+	if (option.value) {
+		parsedOptions[option.full] = option.default;
+	}
+	else {
+		parsedOptions[option.full] = false;
+	}
+}
+
+
+var args = process.argv;
 
 for (var i = 2; i < args.length; ++i) {
 	var arg = args[i];
-	
-	if (arg === "pch") Options.setPrecompiledHeaders(true);
-	else if (arg === 'compile') compile = true;
-	else if (arg === 'run') {
-		compile = true;
-		run = true;
+
+	if (arg[0] == '-') {
+		if (arg[1] == '-') {
+			if (arg.substr(2) === 'help') {
+				printHelp();
+				process.exit(0);
+			}
+			for (var o in options) {
+				var option = options[o];
+				if (arg.substr(2) === option.full) {
+					if (option.value) {
+						++i;
+						parsedOptions[option.full] = args[i];
+					}
+					else {
+						parsedOptions[option.full] = true;
+					}
+				} else
+				if(arg.substr(2).startsWith(option.full + '=')) {
+					if (option.value) {
+						parsedOptions[option.full] = arg.substr(option.full.length + 3); // + 3 because we have two -- and 1 =
+					} else {
+						parsedOptions[option.full] = true;
+					}
+				}
+			}
+		}
+		else {
+			if (arg[1] === 'h') {
+				printHelp();
+				process.exit(0);
+			}
+			for (var o in options) {
+				var option = options[o];
+
+				if (option.short && arg.substr(1) === option.short) {
+					if (option.value) {
+						++i;
+						parsedOptions[option.full] = args[i];
+					} else {
+						parsedOptions[option.full] = true;
+					}
+				} else
+				if(option.short && arg.substr(1).startsWith(option.short + '=')) {
+					if (option.value) {
+						parsedOptions[option.full] = arg.substr(option.short.length + 2); // + 2 because we have one - and 1 =
+					} else {
+						parsedOptions[option.full] = true;
+					}
+				}
+			}
+		}
 	}
-	else if (arg.startsWith("intermediate=")) Options.setIntermediateDrive(arg.substr(13));
-	else if (arg.startsWith("gfx=")) gfx = arg.substr(4);
-	else if (arg.startsWith("vs=")) vs = arg.substr(3);
-
-	else if (arg.startsWith("from=")) from = arg.substr(5);
-	else if (arg.startsWith("to=")) to = arg.substr(3);
-	else if (arg.startsWith("--nokrafix")) nokrafix = true;
-
 	else {
-		platform = arg;
+		parsedOptions.platform = arg;
 	}
 }
 
+if (parsedOptions.graphicsApi === GraphicsApi.OpenGL) {
+	parsedOptions.graphicsApi = GraphicsApi.OpenGL2;
+}
+
+if (parsedOptions.run) {
+	parsedOptions.compile = true;
+}
+
+if(!parsedOptions.emcc && parsedOptions.platform == Platform.HTML5) {
+	console.error("For HTML5 platforms you need to set the emcc path");
+	process.exit(0);
+}
+
+if(parsedOptions.update) {
+	console.log("Updating everything...");
+	require('child_process').spawnSync('git', ['submodule', 'foreach', '--recursive', 'git', 'pull', 'origin', 'master'], { stdio: 'inherit', stderr: 'inherit' });	
+	process.exit(0);
+}
+
 require('./main.js').run(
-{
-	from: from,
-	to: to,
-	platform: platform,
-	graphicsApi: gfx,
-	visualStudioVersion: vs,
-	compile: compile,
-	run: run,
-	nokrafix: nokrafix
-},
+	parsedOptions,
 {
 	info: console.log,
 	error: console.log
