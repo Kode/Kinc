29c67d2efe48df49c965f5da48a01494b00fe75d
Lubos Lenco <lubos.lenco@gmail.com>
2015-10-12 21:29:24 +0200

    Fix WindowsApp compilation

---
+++ Backends/WindowsApp/Sources/Kore/input/Mouse.cpp
@@ -0,0 +1,26 @@
+#include "../pch.h"
+#include <Kore/Input/Mouse.h>
+
+using namespace Kore;
+
+void Mouse::_lock(bool truth){
+	//TODO
+}
+
+
+bool Mouse::canLock(){
+	return false; //FOR NOW
+}
+
+
+void Mouse::show(bool truth){
+	//TODO
+}
+
+void Mouse::setPosition(int x, int y){
+	//TODO
+}
+
+void Mouse::getPosition(int& x, int& y){
+	//TODO
+}
