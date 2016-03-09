3d78fda4a94a51843360381cf1ca5e82ff81014a
David Klein <dave@smokinheads.de>
2016-03-09 12:37:41 +0100

    empty setColorMask() implementations for d3d11 + d3d12

--- Backends/Direct3D12/Sources/Kore/Direct3D12.cpp
+++ Backends/Direct3D12/Sources/Kore/Direct3D12.cpp
@@ -333,6 +333,18 @@ void Graphics::setTextureAddressing(TextureUnit unit, TexDir dir, TextureAddress
 	
 }
 
+// (DK) fancy macro's to generate a clickable warning message in visual studio, can be removed when setColorMask() is implemented
+#define Stringize( L )			#L
+#define MakeString( M, L )		M(L)
+#define $Line					\
+	MakeString( Stringize, __LINE__ )
+#define Warning				\
+	__FILE__ "(" $Line ") : warning: "
+
+void Graphics::setColorMask(bool red, bool green, bool blue, bool alpha) {
+#pragma message(Warning "(DK) Robert, please implement d3d12's version of setColorMask() here")
+}
+
 void Graphics::clear(uint flags, uint color, float depth, int stencil) {
 	
 }
