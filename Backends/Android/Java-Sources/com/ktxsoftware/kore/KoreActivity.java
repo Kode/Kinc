38dde5d1866c0bccd14f7b8517976a40151e9f8e
romamik <vo@romamik.com>
2016-10-04 13:25:37 +0300

    Support for koremake's disableStickyImmersiveMode option.

--- Backends/Android/Java-Sources/com/ktxsoftware/kore/KoreActivity.java
+++ Backends/Android/Java-Sources/com/ktxsoftware/kore/KoreActivity.java
@@ -3,6 +3,8 @@ package com.ktxsoftware.kore;
 import android.app.NativeActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -13,6 +15,7 @@ import android.view.inputmethod.InputMethodManager;
 public class KoreActivity extends NativeActivity {
 	private static KoreActivity instance;
 	private InputMethodManager inputManager;
+    private boolean isDisabledStickyImmersiveMode;
 
 	public static KoreActivity getInstance() {
 		return instance;
@@ -23,19 +26,28 @@ public class KoreActivity extends NativeActivity {
 		super.onCreate(state);
 		instance = this;
 		inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        try {
+            ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            isDisabledStickyImmersiveMode = bundle.getBoolean("disableStickyImmersiveMode");
+        } catch (PackageManager.NameNotFoundException|NullPointerException e) {
+            isDisabledStickyImmersiveMode = false;
+        }
 	}
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
+        if (hasFocus && !isDisabledStickyImmersiveMode) {
             getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
     }
 
 	public static void showKeyboard() {
