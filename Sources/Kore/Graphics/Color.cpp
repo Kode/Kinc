3a86c8bd2eaa41f071fadc5c3fdea580a1a67e78
Polona Caserman <polona.caserman@gmail.com>
2016-10-06 18:13:50 +0200

    Calculate alpha value from hex value correctly

--- Sources/Kore/Graphics/Color.cpp
+++ Sources/Kore/Graphics/Color.cpp
@@ -9,8 +9,8 @@ Color::Color(uint color) {
 }
 
 void Color::getColorFromHex(uint color, float &red, float &green, float &blue, float &alpha) {
-    red   = (color & 0x00ff0000) >> 16;
-    green = (color & 0x0000ff00) >> 8;
-    blue  = (color & 0x000000ff);
-    alpha = (color & 0xff000000);
+    alpha = ((color & 0xff000000) >> 24) / 255.0f;
+    red   = ((color & 0x00ff0000) >> 16) / 255.0f;
+    green = ((color & 0x0000ff00) >> 8) / 255.0f;
+    blue  = (color & 0x000000ff) / 255.0f;
 }
