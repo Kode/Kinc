a81a8a53c772c068349b72aca29785916a77b5ae
Christian Reuter <christian@aoe-maniac.de>
2016-10-26 11:55:30 +0200

    Implemented simple connection state.

--- Sources/Kore/Network/Connection.cpp
+++ Sources/Kore/Network/Connection.cpp
@@ -5,14 +5,26 @@
 #include <cassert>
 #include <cstring>
 
+#include <Kore/System.h>
+
 using namespace Kore;
 
-Connection::Connection(const char* url, int sendPort, int receivePort, int buffSize) : url(url), sndPort(sendPort), recPort(receivePort), buffSize(buffSize) {
+Connection::Connection(const char* url, int sendPort, int receivePort, double timeout, int buffSize) :
+		url(url),
+		sndPort(sendPort),
+		recPort(receivePort),
+		timeout(timeout),
+		buffSize(buffSize) {
+
 	socket.init();
 	socket.open(receivePort);
 
 	sndBuff = new u8[buffSize];
 	recBuff = new u8[buffSize];
+
+	state = Disconnected;
+	lastTime = System::time();
+	lastSndNr = 0;
 	// TODO: (Dis-)connection handling, especially for the server (broadcasting, control messages - client hello / ping) -> maybe split into two classes
 }
 
@@ -22,12 +34,16 @@ Connection::~Connection() {
 }
 
 void Connection::send(const u8* data, int size, bool reliable) {
+	send(data, size, reliable ? Reliable : Unreliable);
+}
+
+void Connection::send(const u8* data, int size, PaketType type) {
 	assert(size + headerSize <= buffSize);
 
 	// TODO: Separate seq nrs for reliable and unreliable (only discarded on old)
 
 	// Identifier
-	*((u32*)(sndBuff)) = magicID;
+	*((u32*)(sndBuff)) = (magicID & 0xFFFFFF00) + type;
 	// Reliability via sequence numbers (wrap around)
 	*((u32*)(sndBuff + 4)) = lastSndNr++;
 
@@ -36,6 +52,7 @@ void Connection::send(const u8* data, int size, bool reliable) {
 	socket.send(url, sndPort, sndBuff, headerSize + size);
 }
 
+// Must be called regularily as it also keeps the connection alive
 int Connection::receive(u8* data) {
 	unsigned int recAddr;
 	unsigned int recPort;
@@ -45,7 +62,13 @@ int Connection::receive(u8* data) {
 
 	if (size >= 0) {
 		// Check for prefix (stray packets)
-		if (*((u32*)(recBuff)) == magicID) {
+		u32 header = *((u32*)(recBuff));
+		if ((header & 0xFFFFFF00) == (magicID & 0xFFFFFF00)) {
+			state = Connected;
+			lastTime = System::time();
+
+			PaketType type = (PaketType)(header & 0x000000FF);
+
 			// TODO: Handle missing packets
 			int recNr = *((u32*)(recBuff + 4));
 
@@ -55,6 +78,10 @@ int Connection::receive(u8* data) {
 			
 			return msgSize;
 		}
+	}
+
+	if (System::time() - lastTime > timeout) {
+		state = Disconnected;
 	}
 
 	return 0;
--- Sources/Kore/Network/Connection.h
+++ Sources/Kore/Network/Connection.h
@@ -6,12 +6,25 @@ namespace Kore {
 
 	class Connection {
 	public:
-		Connection(const char* url, int sendPort, int receivePort, int buffSize = 256);
+		enum State {
+			Disconnected = 0,
+			Connected = 1,
+		};
+
+		State state;
+
+		Connection(const char* url, int sendPort, int receivePort, double timeout = 10, int buffSize = 256);
 		~Connection();
 		void send(const u8* data, int size, bool reliable = true);
 		int receive(u8* data);
 	private:
-		static const u32 magicID = 19285;
+		enum PaketType {
+			Control = 1,
+			Reliable = 2,
+			Unreliable = 3
+		};
+
+		static const u32 magicID = 1346655563;
 		static const int headerSize = 8;
 
 		const int sndPort;
@@ -23,5 +36,10 @@ namespace Kore {
 		u32 lastSndNr;
 		u8* recBuff;
 		u8* sndBuff;
+
+		double timeout;
+		double lastTime;
+
+		void send(const u8* data, int size, PaketType type);
 	};
 }
