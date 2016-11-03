170b2a915a00646a3f60bbecbfa40c6b0f0ba4b2
romamik <vo@romamik.com>
2016-11-03 17:13:55 +0300

    Revert "added method initFromReader, switched Image(filename) constructor to use this method"
    This reverts commit 6618701a591237bd4f2574312663fe406c9c3ba4.

--- Sources/Kore/Graphics/Image.cpp
+++ Sources/Kore/Graphics/Image.cpp
@@ -44,23 +44,11 @@ Image::Image(int width, int height, int depth, Format format, bool readable) : w
 	data = new u8[width * height * depth * sizeOf(format)];
 }
 
-Image::Image(const char* filename, bool readable) {
-	const char* fileExt = strrchr(filename, '.');
-	if (fileExt != nullptr) 
-		++fileExt;
-	else 
-		fileExt = "";
-	FileReader reader(filename);
-	initFromReader(&reader, fileExt, readable);
-}
-
-void Image::initFromReader(Reader* reader, const char* fileExt, bool readable) {
-	this->depth = 1;
-	this->format = RGBA32;
-	this->readable = readable;
-
-	if (0 == strcmp(fileExt, "k")) {
-		u8* data = (u8*)reader->readAll();
+Image::Image(const char* filename, bool readable) : depth(1), format(RGBA32), readable(readable) {
+	printf("Image %s\n", filename);
+	FileReader file(filename);
+	if (endsWith(filename, ".k")) {
+		u8* data = (u8*)file.readAll();
 		width = Reader::readS32LE(data + 0);
 		height = Reader::readS32LE(data + 4);
 		char fourcc[5];
@@ -71,34 +59,34 @@ void Image::initFromReader(Reader* reader, const char* fileExt, bool readable) {
 		fourcc[4] = 0;
 		if (strcmp(fourcc, "SNAP") == 0) {
 			size_t length;
-			snappy::GetUncompressedLength((char*)(data + 12), reader->size() - 12, &length);
+			snappy::GetUncompressedLength((char*)(data + 12), file.size() - 12, &length);
 			this->data = (u8*)malloc(length);
-			snappy::RawUncompress((char*)(data + 12), reader->size() - 12, (char*)this->data);
+			snappy::RawUncompress((char*)(data + 12), file.size() - 12, (char*)this->data);
 		}
 	}
-	else if (0 == strcmp(fileExt, "pvr")) {
-		u32 version = reader->readU32LE();
-		u32 flags = reader->readU32LE();
-		u64 pixelFormat1 = reader->readU64LE();
-		u32 colourSpace = reader->readU32LE();
-		u32 channelType = reader->readU32LE();
-		u32 height = reader->readU32LE();
-		u32 width = reader->readU32LE();
-		u32 depth = reader->readU32LE();
-		u32 numSurfaces = reader->readU32LE();
-		u32 numFaces = reader->readU32LE();
-		u32 mipMapCount = reader->readU32LE();
-		u32 metaDataSize = reader->readU32LE();
+	else if (endsWith(filename, ".pvr")) {
+		u32 version = file.readU32LE();
+		u32 flags = file.readU32LE();
+		u64 pixelFormat1 = file.readU64LE();
+		u32 colourSpace = file.readU32LE();
+		u32 channelType = file.readU32LE();
+		u32 height = file.readU32LE();
+		u32 width = file.readU32LE();
+		u32 depth = file.readU32LE();
+		u32 numSurfaces = file.readU32LE();
+		u32 numFaces = file.readU32LE();
+		u32 mipMapCount = file.readU32LE();
+		u32 metaDataSize = file.readU32LE();
 		
-		u32 meta1fourcc = reader->readU32LE();
-		u32 meta1key = reader->readU32LE();
-		u32 meta1size = reader->readU32LE();
-		u32 meta1data = reader->readU32LE();
+		u32 meta1fourcc = file.readU32LE();
+		u32 meta1key = file.readU32LE();
+		u32 meta1size = file.readU32LE();
+		u32 meta1data = file.readU32LE();
 		
-		u32 meta2fourcc = reader->readU32LE();
-		u32 meta2key = reader->readU32LE();
-		u32 meta2size = reader->readU32LE();
-		u32 meta2data = reader->readU32LE();
+		u32 meta2fourcc = file.readU32LE();
+		u32 meta2key = file.readU32LE();
+		u32 meta2size = file.readU32LE();
+		u32 meta2data = file.readU32LE();
 		
 		int w = 0;
 		int h = 0;
@@ -113,48 +101,48 @@ void Image::initFromReader(Reader* reader, const char* fileExt, bool readable) {
 		compressed = true;
 		internalFormat = 0;
 		
-		u8* all = (u8*)reader->readAll();
+		u8* all = (u8*)file.readAll();
 		dataSize = width * height / 2;
 		data = new u8[dataSize];
 		for (int i = 0; i < dataSize; ++i) {
 			data[i] = all[52 + metaDataSize + i];
 		}
 	}
-	else if (0 == strcmp(fileExt, "astc")) {
-		u32 magic = reader->readU32LE();
-    	u8 blockdim_x = reader->readU8();
-    	u8 blockdim_y = reader->readU8();
-    	u8 blockdim_z = reader->readU8();
+	else if (endsWith(filename, ".astc")) {
+		u32 magic = file.readU32LE();
+    	u8 blockdim_x = file.readU8();
+    	u8 blockdim_y = file.readU8();
+    	u8 blockdim_z = file.readU8();
     	internalFormat = (blockdim_x << 8) + blockdim_y;
     	u8 xsize[4];
-    	xsize[0] = reader->readU8();
-    	xsize[1] = reader->readU8();
-    	xsize[2] = reader->readU8();
+    	xsize[0] = file.readU8();
+    	xsize[1] = file.readU8();
+    	xsize[2] = file.readU8();
     	xsize[3] = 0;
     	this->width = *(unsigned*)&xsize[0];
     	u8 ysize[4];
-    	ysize[0] = reader->readU8();
-    	ysize[1] = reader->readU8();
-    	ysize[2] = reader->readU8();
+    	ysize[0] = file.readU8();
+    	ysize[1] = file.readU8();
+    	ysize[2] = file.readU8();
     	ysize[3] = 0;
     	this->height = *(unsigned*)&ysize[0];
     	u8 zsize[3];
-    	zsize[0] = reader->readU8();
-    	zsize[1] = reader->readU8();
-    	zsize[2] = reader->readU8();
-    	u8* all = (u8*)reader->readAll();
-		dataSize = reader->size() - 16;
+    	zsize[0] = file.readU8();
+    	zsize[1] = file.readU8();
+    	zsize[2] = file.readU8();
+    	u8* all = (u8*)file.readAll();
+		dataSize = file.size() - 16;
 		data = new u8[dataSize];
 		for (int i = 0; i < dataSize; ++i) {
 			data[i] = all[16 + i];
 		}
 	}
-	else if (0 == strcmp(fileExt, "png")) {
-		int size = reader->size();
+	else if (endsWith(filename, ".png")) {
+		int size = file.size();
 		int comp;
 		compressed = false;
 		internalFormat = 0;
-		data = stbi_load_from_memory((u8*)reader->readAll(), size, &width, &height, &comp, 4);
+		data = stbi_load_from_memory((u8*)file.readAll(), size, &width, &height, &comp, 4);
 		for (int y = 0; y < height; ++y) {
 			for (int x = 0; x < width; ++x) {
 				float r = data[y * width * 4 + x * 4 + 0] / 255.0f;
@@ -171,21 +159,21 @@ void Image::initFromReader(Reader* reader, const char* fileExt, bool readable) {
 		}
 		dataSize = width * height * 4;
 	}
-	else if (0 == strcmp(fileExt, "hdr")) {
-		int size = reader->size();
+	else if (endsWith(filename, ".hdr")) {
+		int size = file.size();
 		int comp;
 		compressed = false;
 		internalFormat = 0;
-		hdrData = stbi_loadf_from_memory((u8*)reader->readAll(), size, &width, &height, &comp, 4);
+		hdrData = stbi_loadf_from_memory((u8*)file.readAll(), size, &width, &height, &comp, 4);
 		dataSize = width * height * 16;
 		format = RGBA128;
 	}
 	else {
-		int size = reader->size();
+		int size = file.size();
 		int comp;
 		compressed = false;
 		internalFormat = 0;
-		data = stbi_load_from_memory((u8*)reader->readAll(), size, &width, &height, &comp, 4);
+		data = stbi_load_from_memory((u8*)file.readAll(), size, &width, &height, &comp, 4);
 		dataSize = width * height * 4;
 	}
 }
