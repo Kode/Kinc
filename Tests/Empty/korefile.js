961543ff6a42b6da48c866e915703a4db962111f
Robert Konrad <git2016@robdangero.us>
2016-12-23 14:47:04 +0100

    Update tests

--- Tests/Empty/korefile.js
+++ Tests/Empty/korefile.js
@@ -1,9 +1,9 @@
-let project = new Project('ShaderTest', __dirname);
+let project = new Project('Empty', __dirname);
 
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
 
-Project.createProject('Kore', __dirname).then((kore) => {
+Project.createProject('../../', __dirname).then((kore) => {
 	project.addSubProject(kore);
 	resolve(project);
 });
