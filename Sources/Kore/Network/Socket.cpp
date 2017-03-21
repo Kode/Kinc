4390fa15ab2c07cb6f9003cb6f247259687a413e
Robert Konrad <git2016@robdangero.us>
2017-03-21 11:56:55 +0100

    Fix compilation for Windows

--- Sources/Kore/Network/Socket.cpp
+++ Sources/Kore/Network/Socket.cpp
@@ -194,6 +194,10 @@ void Socket::send(const char* url, int port, const u8* data, int size) {
 #endif
 }
 
+#ifdef SYS_WINDOWS
+typedef int ssize_t;
+#endif
+
 int Socket::receive(u8* data, int maxSize, unsigned& fromAddress, unsigned& fromPort) {
 #if defined(SYS_WINDOWS) || defined(SYS_WINDOWSAPP)
 	typedef int socklen_t;
