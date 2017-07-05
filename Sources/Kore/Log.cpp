16789c22e4f94026b412d7d3839053cb51f689b9
Robert Konrad <git2016@robdangero.us>
2017-07-05 14:46:11 +0200

    Fix log on Windows

--- Sources/Kore/Log.cpp
+++ Sources/Kore/Log.cpp
@@ -18,6 +18,8 @@ using namespace Kore;
 void Kore::log(LogLevel level, const char* format, ...) {
 	va_list args;
 	va_start(args, format);
+	//char* arg = va_arg(args, char*);
+	//float arg2 = va_arg(args, double);
 	logArgs(level, format, args);
 	va_end(args);
 }
@@ -30,24 +32,74 @@ void Kore::logArgs(LogLevel level, const char* format, va_list args) {
 	wchar_t buffer[4096];
 	int bufferIndex = 0;
 	buffer[bufferIndex] = 0;
-
+	printf("");
 	for (int i = 0; formatw[i] != 0; ++i) {
 		if (formatw[i] == L'%') {
 			++i;
-			if (formatw[i] == L's' || formatw[i] == L'S') {
+			switch (formatw[i]) {
+			case L's':
+			case L'S':
+			{
 				char* arg = va_arg(args, char*);
 				wchar_t argw[1024];
 				MultiByteToWideChar(CP_UTF8, 0, arg, -1, argw, 1024);
 				wcscat(buffer, argw);
 				bufferIndex += wcslen(argw);
+				break;
 			}
-			else {
+			case L'd':
+			case L'i':
+			case L'u':
+			case L'o':
+			case L'x':
+			{
+				int arg = va_arg(args, int);
+				wchar_t argformat[3];
+				argformat[0] = L'%';
+				argformat[1] = formatw[i];
+				argformat[2] = 0;
+				bufferIndex += swprintf(&buffer[bufferIndex], 4096 - bufferIndex - 1, argformat, arg);
+				break;
+			}
+			case 'f':
+			case 'e':
+			case 'g':
+			case 'a':
+			{
+				double arg = va_arg(args, double);
+				wchar_t argformat[3];
+				argformat[0] = L'%';
+				argformat[1] = formatw[i];
+				argformat[2] = 0;
+				bufferIndex += swprintf(&buffer[bufferIndex], 4096 - bufferIndex - 1, argformat, arg);
+				break;
+			}
+			case 'c':
+			{
+				char arg = va_arg(args, char);
+				wchar_t argformat[3];
+				argformat[0] = L'%';
+				argformat[1] = formatw[i];
+				argformat[2] = 0;
+				bufferIndex += swprintf(&buffer[bufferIndex], 4096 - bufferIndex - 1, argformat, arg);
+				break;
+			}
+			case 'p':
+			case 'n':
+			{
 				void* arg = va_arg(args, void*);
 				wchar_t argformat[3];
 				argformat[0] = L'%';
 				argformat[1] = formatw[i];
 				argformat[2] = 0;
 				bufferIndex += swprintf(&buffer[bufferIndex], 4096 - bufferIndex - 1, argformat, arg);
+				break;
+			}
+			case '%':
+			{
+				bufferIndex += swprintf(&buffer[bufferIndex], 4096 - bufferIndex - 1, L"%%");
+				break;
+			}
 			}
 		}
 		else {
