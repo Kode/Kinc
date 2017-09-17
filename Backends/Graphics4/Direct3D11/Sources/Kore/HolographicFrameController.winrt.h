4484e4f04ebd861e7619a19328646fb65e5a5d7d
Maximilian Weigel <max@heilenz.net>
2017-09-18 00:19:41 +0200

    link to uwp sample repo

--- Backends/Graphics4/Direct3D11/Sources/Kore/HolographicFrameController.winrt.h
+++ Backends/Graphics4/Direct3D11/Sources/Kore/HolographicFrameController.winrt.h
@@ -1,14 +1,10 @@
 #pragma once
 //*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
+// Original author Microsoft (license MIT)
+// https://github.com/Microsoft/Windows-universal-samples/tree/master/Samples/HolographicSpatialMapping
+// Code is adapted for Kore integration.
 //*********************************************************
+
 #define NOMINMAX
 #include "DeviceResources.winrt.h"
 #include "VideoFrameProcessor.winrt.h"
--- Backends/Graphics4/Direct3D11/Sources/Kore/VideoFrameProcessor.winrt.h
+++ Backends/Graphics4/Direct3D11/Sources/Kore/VideoFrameProcessor.winrt.h
@@ -1,13 +1,9 @@
 //*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
+// Original author Microsoft (license MIT)
+// https://github.com/Microsoft/Windows-universal-samples/tree/master/Samples/HolographicFaceTracking
+// Code is adapted for Kore integration.
 //*********************************************************
+
 #define NOMINMAX
 #pragma once
 #include <concrt.h>
