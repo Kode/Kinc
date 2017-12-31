0483930d78256aa0bc589128d074ac594030031c
Robert Konrad <git2016@robdangero.us>
2017-12-31 01:39:44 +0100

    Fix initial render target clear in Direct3D 12

--- Backends/Graphics4/G4onG5/Sources/Kore/RenderTargetImpl.cpp
+++ Backends/Graphics4/G4onG5/Sources/Kore/RenderTargetImpl.cpp
@@ -14,7 +14,10 @@ Graphics4::RenderTarget::RenderTarget(int width, int height, int depthBufferBits
 	: RenderTargetImpl(width, height, depthBufferBits, antialiasing, (Graphics5::RenderTargetFormat)format, stencilBufferBits, contextId) {
 	this->texWidth = this->width = width;
 	this->texHeight = this->height = height;
-	commandList->clear(&_renderTarget, ClearColorFlag);
+	if (contextId >= 0) {
+		commandList->textureToRenderTargetBarrier(&_renderTarget);
+		commandList->clear(&_renderTarget, ClearColorFlag);
+	}
 }
 
 Graphics4::RenderTarget::RenderTarget(int cubeMapSize, int depthBufferBits, bool antialiasing, RenderTargetFormat format, int stencilBufferBits, int contextId)
