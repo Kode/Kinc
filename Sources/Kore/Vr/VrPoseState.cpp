f6fa067fdd92318cbfb5f6927bc63134ffc46f2f
Polona Caserman <polona.caserman@gmail.com>
2018-01-31 14:57:49 +0100

    Reinsert angular and linear velocity and acceleration

--- Sources/Kore/Vr/VrPoseState.cpp
+++ Sources/Kore/Vr/VrPoseState.cpp
@@ -2,7 +2,9 @@
 
 #include "VrPoseState.h"
 
-VrPoseState::VrPoseState() : trackedDevice(HMD), isVisible(false), hmdPresenting(false), hmdMounted(false),
+VrPoseState::VrPoseState() : angularVelocity(Kore::vec3(0, 0, 0)), linearVelocity(Kore::vec3(0, 0, 0)),
+							 angularAcceleration(Kore::vec3(0, 0, 0)), linearAcceleration(Kore::vec3(0, 0, 0)),
+							 trackedDevice(HMD), isVisible(false), hmdPresenting(false), hmdMounted(false),
 							 displayLost(false), shouldQuit(false), shouldRecenter(false)  {
 
 }
