8bb2e38b65e3b905c59b41cab2afbd7299eaedfa
Polona Caserman <polona.caserman@gmail.com>
2018-01-31 13:56:39 +0100

    Remove angular and linear velocity and acceleration

--- Sources/Kore/Vr/VrPoseState.cpp
+++ Sources/Kore/Vr/VrPoseState.cpp
@@ -2,9 +2,8 @@
 
 #include "VrPoseState.h"
 
-VrPoseState::VrPoseState() : angularVelocity(Kore::vec3(0, 0, 0)), linearVelocity(Kore::vec3(0, 0, 0)),
-							 angularAcceleration(Kore::vec3(0, 0, 0)), linearAcceleration(Kore::vec3(0, 0, 0)),
-							 trackedDevice(HMD) {
+VrPoseState::VrPoseState() : trackedDevice(HMD), isVisible(false), hmdPresenting(false), hmdMounted(false),
+							 displayLost(false), shouldQuit(false), shouldRecenter(false)  {
 
 }
 
--- Sources/Kore/Vr/VrPoseState.h
+++ Sources/Kore/Vr/VrPoseState.h
@@ -11,11 +11,15 @@ public:
 	~VrPoseState();
 
 	VrPose vrPose;
-	Kore::vec3 angularVelocity;				// Angular velocity in radians per second.
-	Kore::vec3 linearVelocity;				// Velocity in meters per second.
-	Kore::vec3 angularAcceleration;			// Angular acceleration in radians per second per second.
-	Kore::vec3 linearAcceleration;			// Acceleration in meters per second per second.
 
 	TrackedDevice trackedDevice;
+
+	// Sensor status
+	bool isVisible;
+	bool hmdPresenting;
+	bool hmdMounted;
+	bool displayLost;
+	bool shouldQuit;
+	bool shouldRecenter;
 };
 
