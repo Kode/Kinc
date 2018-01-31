980fcbad48ccb1ccc650127236a8c4900d9d2939
Polona Caserman <polona.caserman@gmail.com>
2018-01-31 13:57:43 +0100

    Move sensor status to VrPoseState

--- Sources/Kore/Vr/SensorState.cpp
+++ Sources/Kore/Vr/SensorState.cpp
@@ -2,8 +2,7 @@
 
 #include "SensorState.h"
 
-SensorState::SensorState() : isVisible(false), hmdPresenting(false), hmdMounted(false), 
-							 displayLost(false), shouldQuit(false), shouldRecenter(false) {
+SensorState::SensorState() {
 
 }
 
