dda2c5d423080e7cd16db148339a9b3871399378
Robert Konrad <git2016@robdangero.us>
2018-04-27 17:37:29 +0200

    Show error message boxes on Windows

--- Sources/Kore/Error.cpp
+++ Sources/Kore/Error.cpp
@@ -32,15 +32,46 @@ void Kore::error() {
 	error("Unknown error");
 }
 
+#if defined(KORE_WINDOWS) || defined(KORE_WINDOWSAPP) || defined(KORE_XBOX_ONE)
+#include <Windows.h>
+
+namespace Kore {
+	namespace Windows {
+		void format(const char* format, va_list args, wchar_t* buffer);
+	}
+}
+#endif
+
 void Kore::error(const char* format, ...) {
-	va_list args;
-	va_start(args, format);
-	logArgs(Error, format, args);
-	va_end(args);
+	{
+		va_list args;
+		va_start(args, format);
+		logArgs(Error, format, args);
+		va_end(args);
+	}
+
+#if defined(KORE_WINDOWS) || defined(KORE_WINDOWSAPP)
+	{
+		va_list args;
+		va_start(args, format);
+		wchar_t buffer[4096];
+		Windows::format(format, args, buffer);
+		MessageBox(nullptr, buffer, L"Error", 0);
+		va_end(args);
+	}
+#endif
+
 	exit(EXIT_FAILURE);
 }
 
 void Kore::errorArgs(const char* format, va_list args) {
 	logArgs(Error, format, args);
+
+#if defined(KORE_WINDOWS) || defined(KORE_WINDOWSAPP)
+	wchar_t buffer[4096];
+	Windows::format(format, args, buffer);
+	MessageBox(nullptr, buffer, L"Error", 0);
+#endif
+
 	exit(EXIT_FAILURE);
 }
