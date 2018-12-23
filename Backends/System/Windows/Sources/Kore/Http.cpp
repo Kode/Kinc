67f843e6dada5dd8bd58167f24ae852b36d921e4
Robert Konrad <git2016@robdangero.us>
2018-12-23 18:26:40 +0100

    Send all requested data on Windows http requests

--- Backends/System/Windows/Sources/Kore/Http.cpp
+++ Backends/System/Windows/Sources/Kore/Http.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 
 #include <Kore/Network/Http.h>
+#include <Kore/Log.h>
 
 #include <stdio.h>
 
@@ -37,41 +38,54 @@ void Kore::httpRequest(const char* url, const char* path, const char* data, int 
 
 	hSession = WinHttpOpen(L"WinHTTP via Kore/1.0", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
 
-	wchar_t wurl[4096];
-	MultiByteToWideChar(CP_UTF8, 0, url, -1, wurl, 4096);
+	if (hSession) {
+		wchar_t wurl[4096];
+		MultiByteToWideChar(CP_UTF8, 0, url, -1, wurl, 4096);
+		hConnect = WinHttpConnect(hSession, wurl, port, 0);
+	}
 
-	if (hSession) hConnect = WinHttpConnect(hSession, wurl, INTERNET_DEFAULT_HTTPS_PORT, 0);
+	if (hConnect) {
+		wchar_t wpath[4096];
+		MultiByteToWideChar(CP_UTF8, 0, path, -1, wpath, 4096);
+		hRequest =
+		    WinHttpOpenRequest(hConnect, convert(method), wpath, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, secure ? WINHTTP_FLAG_SECURE : 0);
+	}
 
-	if (hConnect) hRequest = WinHttpOpenRequest(hConnect, convert(method), NULL, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, WINHTTP_FLAG_SECURE);
-
-	if (hRequest) bResults = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+	if (hRequest) {
+		wchar_t wheader[4096];
+		if (header) {
+			MultiByteToWideChar(CP_UTF8, 0, header, -1, wheader, 4096);
+		}
+		DWORD optionalLength = (data != 0 && strlen(data) > 0) ? (DWORD)strlen(data) + 1 : 0;
+		bResults = WinHttpSendRequest(hRequest, header == 0 ? WINHTTP_NO_ADDITIONAL_HEADERS : wheader, header == 0 ? 0 : -1L, data == 0 ? WINHTTP_NO_REQUEST_DATA : (LPVOID)data, optionalLength, optionalLength, 0);
+	}
 
 	if (bResults) bResults = WinHttpReceiveResponse(hRequest, NULL);
 
 	if (bResults) {
 		do {
 			dwSize = 0;
-			if (!WinHttpQueryDataAvailable(hRequest, &dwSize)) printf("Error %u in WinHttpQueryDataAvailable.\n", GetLastError());
+			if (!WinHttpQueryDataAvailable(hRequest, &dwSize)) log(Error, "Error %d in WinHttpQueryDataAvailable.\n", GetLastError());
 
 			pszOutBuffer = new char[dwSize + 1];
 			if (!pszOutBuffer) {
-				printf("Out of memory\n");
+				log(Error, "Out of memory\n");
 				dwSize = 0;
 			}
 			else {
 				ZeroMemory(pszOutBuffer, dwSize + 1);
 
 				if (!WinHttpReadData(hRequest, (LPVOID)pszOutBuffer, dwSize, &dwDownloaded))
-					printf("Error %u in WinHttpReadData.\n", GetLastError());
+					log(Error, "Error %d in WinHttpReadData.\n", GetLastError());
 				else
-					printf("%s", pszOutBuffer);
+					log(Info, "%s", pszOutBuffer);
 
 				delete[] pszOutBuffer;
 			}
 		} while (dwSize > 0);
 	}
 
-	if (!bResults) printf("Error %d has occurred.\n", GetLastError());
+	if (!bResults) log(Error, "Error %d has occurred.\n", GetLastError());
 
 	if (hRequest) WinHttpCloseHandle(hRequest);
 	if (hConnect) WinHttpCloseHandle(hConnect);
