43ba069a8165e0b9d4f595d45eb46744e6074459
Robert Konrad <git2016@robdangero.us>
2018-12-23 18:43:35 +0100

    Call the http callback in Windows

--- Backends/System/Windows/Sources/Kore/Http.cpp
+++ Backends/System/Windows/Sources/Kore/Http.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 
-#include <Kore/Network/Http.h>
 #include <Kore/Log.h>
+#include <Kore/Network/Http.h>
 
 #include <stdio.h>
 
@@ -21,6 +21,9 @@ namespace {
 			return L"DELETE";
 		}
 	}
+
+	char* returnData = nullptr;
+	int returnDataSize = 0;
 }
 
 #include <Windows.h>
@@ -29,21 +32,17 @@ namespace {
 void Kore::httpRequest(const char* url, const char* path, const char* data, int port, bool secure, HttpMethod method, const char* header, HttpCallback callback,
                        void* callbackdata) {
 	// based on https://docs.microsoft.com/en-us/windows/desktop/winhttp/winhttp-sessions-overview
+	
+	HINTERNET hSession = WinHttpOpen(L"WinHTTP via Kore/1.0", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
 
-	DWORD dwSize = 0;
-	DWORD dwDownloaded = 0;
-	LPSTR pszOutBuffer;
-	BOOL bResults = FALSE;
-	HINTERNET hSession = NULL, hConnect = NULL, hRequest = NULL;
-
-	hSession = WinHttpOpen(L"WinHTTP via Kore/1.0", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
-
+	HINTERNET hConnect = NULL;
 	if (hSession) {
 		wchar_t wurl[4096];
 		MultiByteToWideChar(CP_UTF8, 0, url, -1, wurl, 4096);
 		hConnect = WinHttpConnect(hSession, wurl, port, 0);
 	}
 
+	HINTERNET hRequest = NULL;
 	if (hConnect) {
 		wchar_t wpath[4096];
 		MultiByteToWideChar(CP_UTF8, 0, path, -1, wpath, 4096);
@@ -51,37 +50,43 @@ void Kore::httpRequest(const char* url, const char* path, const char* data, int 
 		    WinHttpOpenRequest(hConnect, convert(method), wpath, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, secure ? WINHTTP_FLAG_SECURE : 0);
 	}
 
+	BOOL bResults = FALSE;
+
 	if (hRequest) {
 		wchar_t wheader[4096];
 		if (header) {
 			MultiByteToWideChar(CP_UTF8, 0, header, -1, wheader, 4096);
 		}
 		DWORD optionalLength = (data != 0 && strlen(data) > 0) ? (DWORD)strlen(data) + 1 : 0;
-		bResults = WinHttpSendRequest(hRequest, header == 0 ? WINHTTP_NO_ADDITIONAL_HEADERS : wheader, header == 0 ? 0 : -1L, data == 0 ? WINHTTP_NO_REQUEST_DATA : (LPVOID)data, optionalLength, optionalLength, 0);
+		bResults = WinHttpSendRequest(hRequest, header == 0 ? WINHTTP_NO_ADDITIONAL_HEADERS : wheader, header == 0 ? 0 : -1L,
+		                              data == 0 ? WINHTTP_NO_REQUEST_DATA : (LPVOID)data, optionalLength, optionalLength, 0);
 	}
 
 	if (bResults) bResults = WinHttpReceiveResponse(hRequest, NULL);
 
+	int returnDataIndex = 0;
 	if (bResults) {
+		DWORD dwSize;
 		do {
 			dwSize = 0;
 			if (!WinHttpQueryDataAvailable(hRequest, &dwSize)) log(Error, "Error %d in WinHttpQueryDataAvailable.\n", GetLastError());
 
-			pszOutBuffer = new char[dwSize + 1];
-			if (!pszOutBuffer) {
-				log(Error, "Out of memory\n");
-				dwSize = 0;
+			if ((int)dwSize + 1 > returnDataSize - returnDataIndex) {
+				int newReturnDataSize = (returnDataIndex + dwSize + 1) * 2;
+				char* newReturnData = (char*)malloc(newReturnDataSize);
+				if (newReturnData == 0) {
+					log(Error, "Out of memory\n");
+				}
+				memcpy(newReturnData, returnData, returnDataSize);
+				returnDataSize = newReturnDataSize;
+				returnData = newReturnData;
 			}
-			else {
-				ZeroMemory(pszOutBuffer, dwSize + 1);
 
-				if (!WinHttpReadData(hRequest, (LPVOID)pszOutBuffer, dwSize, &dwDownloaded))
-					log(Error, "Error %d in WinHttpReadData.\n", GetLastError());
-				else
-					log(Info, "%s", pszOutBuffer);
-
-				delete[] pszOutBuffer;
-			}
+			DWORD dwDownloaded = 0;
+			if (!WinHttpReadData(hRequest, (LPVOID)(&returnData[returnDataIndex]), dwSize, &dwDownloaded))
+				log(Error, "Error %d in WinHttpReadData.\n", GetLastError());
+			
+			returnDataIndex += dwSize;
 		} while (dwSize > 0);
 	}
 
@@ -90,4 +95,6 @@ void Kore::httpRequest(const char* url, const char* path, const char* data, int 
 	if (hRequest) WinHttpCloseHandle(hRequest);
 	if (hConnect) WinHttpCloseHandle(hConnect);
 	if (hSession) WinHttpCloseHandle(hSession);
+
+	callback(0, 200, returnData, callbackdata);
 }
