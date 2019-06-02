18c2872942fc09ee35354048a3f89687c434c0b9
Robert Konrad <git2016@robdangero.us>
2019-06-03 01:26:41 +0200

    Add empty posix atomics implementation

---
+++ Backends/System/POSIX/Sources/Kore/AtomicImpl.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define KINC_ATOMIC_COMPARE_EXCHANGE(pointer, oldValue, newValue) 0
+
+#define KINC_ATOMIC_COMPARE_EXCHANGE_POINTER(pointer, oldValue, newValue) 0
+
+#define KINC_ATOMIC_INCREMENT(pointer) 0
+
+#define KINC_ATOMIC_DECREMENT(pointer) 0
+
