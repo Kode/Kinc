d16a0983fcc73d61425a09187497ae43f1b1c0c3
Robert Konrad <git2016@robdangero.us>
2019-06-03 04:37:04 +0200

    Implement atomics for posix systems

--- Backends/System/POSIX/Sources/Kore/AtomicImpl.h
+++ Backends/System/POSIX/Sources/Kore/AtomicImpl.h
@@ -1,10 +1,27 @@
 #pragma once
 
-#define KINC_ATOMIC_COMPARE_EXCHANGE(pointer, oldValue, newValue) 0
+#if defined(KORE_MACOS) || defined(KORE_IOS)
 
-#define KINC_ATOMIC_COMPARE_EXCHANGE_POINTER(pointer, oldValue, newValue) 0
+#include <libkern/OSAtomic.h>
 
-#define KINC_ATOMIC_INCREMENT(pointer) 0
+#define KINC_ATOMIC_COMPARE_EXCHANGE(pointer, oldValue, newValue) OSAtomicCompareAndSwap32Barrier(oldValue, newValue, pointer)
 
-#define KINC_ATOMIC_DECREMENT(pointer) 0
+#define KINC_ATOMIC_COMPARE_EXCHANGE_POINTER(pointer, oldValue, newValue) OSAtomicCompareAndSwapPtrBarrier(oldValue, newValue, pointer)
 
+#define KINC_ATOMIC_INCREMENT(pointer) (OSAtomicIncrement32Barrier(pointer) - 1)
+
+#define KINC_ATOMIC_DECREMENT(pointer) (OSAtomicDecrement32Barrier(ioWhere) + 1)
+
+#else
+
+// clang/gcc intrinsics
+
+#define KORE_ATOMIC_COMPARE_EXCHANGE(pointer, oldValue, newValue) (__sync_bool_compare_and_swap(pointer, oldValue, newValue))
+
+#define KORE_ATOMIC_COMPARE_EXCHANGE_POINTER(pointer, oldValue, newValue) (__sync_bool_compare_and_swap(pointer, oldValue, newValue))
+
+#define KORE_ATOMIC_INCREMENT(pointer) (__sync_fetch_and_add(pointer, 1))
+
+#define KORE_ATOMIC_DECREMENT(pointer) (__sync_fetch_and_sub(pointer, 1))
+
+#endif
