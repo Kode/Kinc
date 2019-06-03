375b84e7f8a99608955cc26bda4040887abdfd9c
Robert Konrad <git2016@robdangero.us>
2019-06-03 02:00:42 +0200

    Update G4onG5

--- Backends/Graphics4/G4onG5/Sources/Kore/TextureArrayImpl.c
+++ Backends/Graphics4/G4onG5/Sources/Kore/TextureArrayImpl.c
@@ -2,6 +2,8 @@
 
 #include <Kinc/Graphics4/TextureArray.h>
 
-void kinc_g4_texture_array_init(kinc_g4_texture_array_t *array, kinc_image_t **textures, int count) {}
+void kinc_g4_texture_array_init(kinc_g4_texture_array_t *array, kinc_image_t *textures, int count) {}
+
+void kinc_g4_texture_array_destroy(kinc_g4_texture_array_t *array) {}
 
 //void TextureArrayImpl::set(TextureUnit unit) {}
