9304d7bc0994a85eac1ce6ab6f9f47b5655b7b6b
Robert Konrad <git2016@robdangero.us>
2019-06-21 22:00:22 +0200

    Fix Kore render targets

--- Sources/Kore/Graphics4/RenderTarget.cpp
+++ Sources/Kore/Graphics4/RenderTarget.cpp
@@ -7,14 +7,26 @@
 using namespace Kore;
 using namespace Kore::Graphics4;
 
+static void copyValues(RenderTarget* renderTarget, kinc_g4_render_target_t* kincRenderTarget) {
+	renderTarget->width = kincRenderTarget->width;
+	renderTarget->height = kincRenderTarget->height;
+	renderTarget->texWidth = kincRenderTarget->texWidth;
+	renderTarget->texHeight = kincRenderTarget->texHeight;
+	renderTarget->contextId = kincRenderTarget->contextId;
+	renderTarget->isCubeMap = kincRenderTarget->isCubeMap;
+	renderTarget->isDepthAttachment = kincRenderTarget->isDepthAttachment;
+}
+
 RenderTarget::RenderTarget(int width, int height, int depthBufferBits, bool antialiasing, RenderTargetFormat format, int stencilBufferBits, int contextId) {
 	kinc_g4_render_target_init(&kincRenderTarget, width, height, depthBufferBits, antialiasing, (kinc_g4_render_target_format_t)format, stencilBufferBits,
 	                           contextId);
+	copyValues(this, &kincRenderTarget);
 }
 
 RenderTarget::RenderTarget(int cubeMapSize, int depthBufferBits, bool antialiasing, RenderTargetFormat format, int stencilBufferBits, int contextId) {
 	kinc_g4_render_target_init_cube(&kincRenderTarget, cubeMapSize, depthBufferBits, antialiasing, (kinc_g4_render_target_format_t)format, stencilBufferBits,
 	                                contextId);
+	copyValues(this, &kincRenderTarget);
 }
 
 RenderTarget::~RenderTarget() {
