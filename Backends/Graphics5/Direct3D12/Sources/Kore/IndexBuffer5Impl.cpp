9b652a8b184c4c7374f7179d43d62cb6da24edd1
Robert Konrad <rkonrad@deck13.com>
2019-07-16 18:50:25 +0200

    Fix D3D12 index buffers in release mode

--- Backends/Graphics5/Direct3D12/Sources/Kore/IndexBuffer5Impl.cpp
+++ Backends/Graphics5/Direct3D12/Sources/Kore/IndexBuffer5Impl.cpp
@@ -18,13 +18,13 @@ void kinc_g5_index_buffer_init(kinc_g5_index_buffer_t *buffer, int count, bool g
 	static_assert(sizeof(D3D12IindexBufferView) == sizeof(D3D12_INDEX_BUFFER_VIEW), "Something is wrong with D3D12IindexBufferView");
 	int uploadBufferSize = sizeof(int) * count;
 
-	assert(device->CreateCommittedResource(&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD), D3D12_HEAP_FLAG_NONE, &CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize),
-	                                D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_GRAPHICS_PPV_ARGS(&buffer->impl.uploadBuffer)) == S_OK);
+	kinc_microsoft_affirm(device->CreateCommittedResource(&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD), D3D12_HEAP_FLAG_NONE, &CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize),
+	                                D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_GRAPHICS_PPV_ARGS(&buffer->impl.uploadBuffer)));
 
 	if (gpuMemory) {
-		assert(device->CreateCommittedResource(&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT), D3D12_HEAP_FLAG_NONE,
+		kinc_microsoft_affirm(device->CreateCommittedResource(&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT), D3D12_HEAP_FLAG_NONE,
 		                                &CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize), D3D12_RESOURCE_STATE_COPY_DEST, nullptr,
-		                                IID_GRAPHICS_PPV_ARGS(&buffer->impl.indexBuffer)) == S_OK);
+		                                IID_GRAPHICS_PPV_ARGS(&buffer->impl.indexBuffer)));
 
 		buffer->impl.indexBufferView.BufferLocation = buffer->impl.indexBuffer->GetGPUVirtualAddress();
 	}
@@ -36,8 +36,8 @@ void kinc_g5_index_buffer_init(kinc_g5_index_buffer_t *buffer, int count, bool g
 }
 
 void kinc_g5_index_buffer_destroy(kinc_g5_index_buffer_t *buffer) {
-	// ib->Release();
-	// delete[] indices;
+	buffer->impl.indexBuffer->Release();
+	buffer->impl.uploadBuffer->Release();
 }
 
 int *kinc_g5_index_buffer_lock(kinc_g5_index_buffer_t *buffer) {
