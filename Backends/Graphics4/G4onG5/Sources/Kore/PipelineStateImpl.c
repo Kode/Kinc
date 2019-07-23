e575f7b7fca6a0cfdb30ba442cfb7aca8436db15
luboslenco <lubos.lenco@gmail.com>
2019-07-23 11:04:01 +0200

    Pass color write mask in G4onG5

--- Backends/Graphics4/G4onG5/Sources/Kore/PipelineStateImpl.c
+++ Backends/Graphics4/G4onG5/Sources/Kore/PipelineStateImpl.c
@@ -34,8 +34,7 @@ void kinc_g4_pipeline_compile(kinc_g4_pipeline_t *pipe) {
 	pipe->impl._pipeline.fragmentShader = &pipe->fragment_shader->impl._shader;
 	pipe->impl._pipeline.geometryShader = pipe->geometry_shader != NULL ? &pipe->geometry_shader->impl._shader : NULL;
 	pipe->impl._pipeline.tessellationControlShader = pipe->tessellation_control_shader != NULL ? &pipe->tessellation_control_shader->impl._shader : NULL;
-	pipe->impl._pipeline.tessellationEvaluationShader =
-	    pipe->tessellation_evaluation_shader != NULL ? &pipe->tessellation_evaluation_shader->impl._shader : NULL;
+	pipe->impl._pipeline.tessellationEvaluationShader = pipe->tessellation_evaluation_shader != NULL ? &pipe->tessellation_evaluation_shader->impl._shader : NULL;
 	pipe->impl._pipeline.blendSource = (kinc_g5_blending_operation_t)pipe->blend_source;
 	pipe->impl._pipeline.blendDestination = (kinc_g5_blending_operation_t)pipe->blend_destination;
 	pipe->impl._pipeline.alphaBlendSource = (kinc_g5_blending_operation_t)pipe->alpha_blend_source;
@@ -43,5 +42,11 @@ void kinc_g4_pipeline_compile(kinc_g4_pipeline_t *pipe) {
 	pipe->impl._pipeline.cullMode = (kinc_g5_cull_mode_t)pipe->cull_mode;
 	pipe->impl._pipeline.depthMode = (kinc_g5_compare_mode_t)pipe->depth_mode;
 	pipe->impl._pipeline.depthWrite = pipe->depth_write;
+	for (int i = 0; i < 8; ++i) {
+		pipe->impl._pipeline.colorWriteMaskRed[i] = pipe->color_write_mask_red[i];
+		pipe->impl._pipeline.colorWriteMaskGreen[i] = pipe->color_write_mask_green[i];
+		pipe->impl._pipeline.colorWriteMaskBlue[i] = pipe->color_write_mask_blue[i];
+		pipe->impl._pipeline.colorWriteMaskAlpha[i] = pipe->color_write_mask_alpha[i];
+	}
 	kinc_g5_pipeline_compile(&pipe->impl._pipeline);
 }
