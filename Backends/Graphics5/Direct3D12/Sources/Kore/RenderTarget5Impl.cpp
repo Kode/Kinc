35e8aa8e96260a0f34f85caca882260574800f32
luboslenco <lubos.lenco@gmail.com>
2019-07-24 16:46:55 +0200

    Set depth stencil from for D3D12

--- Backends/Graphics5/Direct3D12/Sources/Kore/RenderTarget5Impl.cpp
+++ Backends/Graphics5/Direct3D12/Sources/Kore/RenderTarget5Impl.cpp
@@ -217,4 +217,8 @@ void kinc_g5_render_target_use_depth_as_texture(kinc_g5_render_target_t *render_
 	currentTextures[render_target->impl.stage_depth] = nullptr;
 }
 
-void kinc_g5_render_target_set_depth_stencil_from(kinc_g5_render_target_t *render_target, kinc_g5_render_target_t *source) {}
+void kinc_g5_render_target_set_depth_stencil_from(kinc_g5_render_target_t *render_target, kinc_g5_render_target_t *source) {
+	render_target->impl.depthStencilDescriptorHeap = source->impl.depthStencilDescriptorHeap;
+	render_target->impl.srvDepthDescriptorHeap = source->impl.srvDepthDescriptorHeap;
+	render_target->impl.depthStencilTexture = source->impl.depthStencilTexture;
+}
