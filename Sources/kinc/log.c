b82b221fa28ee0e6c3249d6efab9bb388bca9afb
RblSb <msrblsb@gmail.com>
2019-08-13 16:02:23 +0300

    Log tag for android

--- Sources/kinc/log.c
+++ Sources/kinc/log.c
@@ -43,13 +43,13 @@ void kinc_log_args(kinc_log_level_t level, const char *format, va_list args) {
 #ifdef KORE_ANDROID
 	switch (level) {
 	case KINC_LOG_LEVEL_INFO:
-		__android_log_vprint(ANDROID_LOG_INFO, kinc_application_name(), format, args);
+		__android_log_vprint(ANDROID_LOG_INFO, "kinc", format, args);
 		break;
 	case KINC_LOG_LEVEL_WARNING:
-		__android_log_vprint(ANDROID_LOG_WARN, kinc_application_name(), format, args);
+		__android_log_vprint(ANDROID_LOG_WARN, "kinc", format, args);
 		break;
 	case KINC_LOG_LEVEL_ERROR:
-		__android_log_vprint(ANDROID_LOG_ERROR, kinc_application_name(), format, args);
+		__android_log_vprint(ANDROID_LOG_ERROR, "kinc", format, args);
 		break;
 	}
 #endif
