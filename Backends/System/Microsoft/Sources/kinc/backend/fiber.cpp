505ba490a8e77cff3685164d01e429dfb6ed3e6b
Robert Konrad <git2016@robdangero.us>
2019-11-02 18:09:44 +0100

    Add a UWP action

--- Backends/System/Microsoft/Sources/kinc/backend/fiber.cpp
+++ Backends/System/Microsoft/Sources/kinc/backend/fiber.cpp
@@ -5,25 +5,35 @@
 #include <Windows.h>
 
 VOID WINAPI fiber_func(LPVOID param) {
+#ifndef KORE_WINDOWSAPP
 	kinc_fiber_t *fiber = (kinc_fiber_t *)param;
 	fiber->impl.func(fiber->impl.param);
+#endif
 }
 
 void kinc_fiber_init_current_thread(kinc_fiber_t *fiber) {
+#ifndef KORE_WINDOWSAPP
 	fiber->impl.fiber = ConvertThreadToFiber(NULL);
+#endif
 }
 
 void kinc_fiber_init(kinc_fiber_t* fiber, void (*func)(void* param), void* param) {
+#ifndef KORE_WINDOWSAPP
 	fiber->impl.func = func;
 	fiber->impl.param = param;
 	fiber->impl.fiber = CreateFiber(0, fiber_func, fiber);
+#endif
 }
 
 void kinc_fiber_destroy(kinc_fiber_t* fiber) {
+#ifndef KORE_WINDOWSAPP
 	DeleteFiber(fiber->impl.fiber);
 	fiber->impl.fiber = NULL;
+#endif
 }
 
 void kinc_fiber_switch(kinc_fiber_t* fiber) {
+#ifndef KORE_WINDOWSAPP
 	SwitchToFiber(fiber->impl.fiber);
+#endif
 }
