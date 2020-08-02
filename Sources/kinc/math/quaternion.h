ca2ff1cc731679f012815dc9b58d74827df3ab37
luboslenco <lubos.lenco@gmail.com>
2020-08-02 09:33:02 +0200

    Add kinc_quaternion_t

---
+++ Sources/kinc/math/quaternion.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+	float w;
+} kinc_quaternion_t;
+
+#ifdef __cplusplus
+}
+#endif
