2a5dd8c603ae3f4e3a4009f3f5af3950c1e90ace
Robert Konrad <git2016@robdangero.us>
2020-10-20 21:14:13 +0200

    Move the socket implementation to C

--- Sources/kinc/network/socket.c
+++ Sources/kinc/network/socket.c
@@ -28,8 +28,8 @@ static void destroy() {
 
 #if defined(KORE_WINDOWS) || defined(KORE_WINDOWSAPP) || defined(KORE_POSIX)
 // Important: Must be cleaned with freeaddrinfo(address) later if the result is 0 in order to prevent memory leaks
-static int resolveAddress(const char* url, int port, addrinfo** result) {
-	addrinfo hints = {};
+static int resolveAddress(const char *url, int port, struct addrinfo **result) {
+	struct addrinfo hints = {0};
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_DGRAM;
 	hints.ai_protocol = IPPROTO_UDP;
@@ -68,20 +68,20 @@ bool kinc_socket_open(kinc_socket_t *sock, int port) {
 			break;
 		case (WSAEAFNOSUPPORT):
 			kinc_log(KINC_LOG_LEVEL_ERROR,
-			          "The specified address family is not supported.For example, an application tried to create a socket for the AF_IRDA address "
-			                 "family but an infrared adapter and device driver is not installed on the local computer.");
+			         "The specified address family is not supported.For example, an application tried to create a socket for the AF_IRDA address "
+			         "family but an infrared adapter and device driver is not installed on the local computer.");
 			break;
 		case (WSAEINPROGRESS):
 			kinc_log(KINC_LOG_LEVEL_ERROR,
-			          "A blocking Windows Sockets 1.1 call is in progress, or the service provider is still processing a callback function.");
+			         "A blocking Windows Sockets 1.1 call is in progress, or the service provider is still processing a callback function.");
 			break;
 		case (WSAEMFILE):
 			kinc_log(KINC_LOG_LEVEL_ERROR, "No more socket descriptors are available.");
 			break;
 		case (WSAEINVAL):
 			kinc_log(KINC_LOG_LEVEL_ERROR,
-			          "An invalid argument was supplied.This error is returned if the af parameter is set to AF_UNSPEC and the type and protocol "
-			                 "parameter are unspecified.");
+			         "An invalid argument was supplied.This error is returned if the af parameter is set to AF_UNSPEC and the type and protocol "
+			         "parameter are unspecified.");
 			break;
 		case (WSAENOBUFS):
 			kinc_log(KINC_LOG_LEVEL_ERROR, "No buffer space is available.The socket cannot be created.");
@@ -94,8 +94,8 @@ bool kinc_socket_open(kinc_socket_t *sock, int port) {
 			break;
 		case (WSAEPROVIDERFAILEDINIT):
 			kinc_log(KINC_LOG_LEVEL_ERROR,
-			          "The service provider failed to initialize.This error is returned if a layered service provider(LSP) or namespace provider was "
-			                 "improperly installed or the provider fails to operate correctly.");
+			         "The service provider failed to initialize.This error is returned if a layered service provider(LSP) or namespace provider was "
+			         "improperly installed or the provider fails to operate correctly.");
 			break;
 		case (WSAESOCKTNOSUPPORT):
 			kinc_log(KINC_LOG_LEVEL_ERROR, "The specified socket type is not supported in this address family.");
@@ -113,11 +113,11 @@ bool kinc_socket_open(kinc_socket_t *sock, int port) {
 		return false;
 	}
 
-	sockaddr_in address;
+	struct sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons((unsigned short)port);
-	if (bind(sock->handle, (const sockaddr*)&address, sizeof(sockaddr_in)) < 0) {
+	if (bind(sock->handle, (const struct sockaddr *)&address, sizeof(struct sockaddr_in)) < 0) {
 		kinc_log(KINC_LOG_LEVEL_ERROR, "Could not bind socket.");
 		return false;
 	}
@@ -152,14 +152,14 @@ void kinc_socket_destroy(kinc_socket_t *sock) {
 
 unsigned kinc_url_to_int(const char *url, int port) {
 #if defined(KORE_WINDOWS) || defined(KORE_WINDOWSAPP)
-	addrinfo* address = nullptr;
+	struct addrinfo *address = NULL;
 	int res = resolveAddress(url, port, &address);
 	if (res != 0) {
 		kinc_log(KINC_LOG_LEVEL_ERROR, "Could not resolve address.");
 		return -1;
 	}
 
-	unsigned fromAddress = ntohl(((sockaddr_in*)address->ai_addr)->sin_addr.S_un.S_addr);
+	unsigned fromAddress = ntohl(((struct sockaddr_in *)address->ai_addr)->sin_addr.S_un.S_addr);
 	freeaddrinfo(address);
 
 	return fromAddress;
@@ -170,12 +170,12 @@ unsigned kinc_url_to_int(const char *url, int port) {
 
 void kinc_socket_send(kinc_socket_t *sock, unsigned address, int port, const unsigned char *data, int size) {
 #if defined(KORE_WINDOWS) || defined(KORE_WINDOWSAPP) || defined(KORE_POSIX)
-	sockaddr_in addr;
+	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = htonl(address);
 	addr.sin_port = htons(port);
 
-	size_t sent = sendto(sock->handle, (const char*)data, size, 0, (sockaddr*)&addr, sizeof(sockaddr_in));
+	size_t sent = sendto(sock->handle, (const char *)data, size, 0, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
 	if (sent != size) {
 		kinc_log(KINC_LOG_LEVEL_ERROR, "Could not send packet.");
 	}
@@ -184,14 +184,14 @@ void kinc_socket_send(kinc_socket_t *sock, unsigned address, int port, const uns
 
 void kinc_socket_send_url(kinc_socket_t *sock, const char *url, int port, const unsigned char *data, int size) {
 #if defined(KORE_WINDOWS) || defined(KORE_WINDOWSAPP) || defined(KORE_POSIX)
-	addrinfo* address = nullptr;
+	struct addrinfo *address = NULL;
 	int res = resolveAddress(url, port, &address);
 	if (res != 0) {
 		kinc_log(KINC_LOG_LEVEL_ERROR, "Could not resolve address.");
 		return;
 	}
 
-	size_t sent = sendto(sock->handle, (const char*)data, size, 0, address->ai_addr, sizeof(sockaddr_in));
+	size_t sent = sendto(sock->handle, (const char *)data, size, 0, address->ai_addr, sizeof(struct sockaddr_in));
 	if (sent != size) {
 		kinc_log(KINC_LOG_LEVEL_ERROR, "Could not send packet.");
 	}
@@ -215,13 +215,15 @@ int kinc_socket_receive(kinc_socket_t *sock, unsigned char *data, int maxSize, u
 	typedef int ssize_t;
 #endif
 #if defined(KORE_WINDOWS) || defined(KORE_WINDOWSAPP) || defined(KORE_POSIX)
-	sockaddr_in from;
+	struct sockaddr_in from;
 	socklen_t fromLength = sizeof(from);
-	ssize_t bytes = recvfrom(sock->handle, (char*)data, maxSize, 0, (sockaddr*)&from, &fromLength);
-	if (bytes <= 0) return static_cast<int>(bytes);
+	ssize_t bytes = recvfrom(sock->handle, (char *)data, maxSize, 0, (struct sockaddr *)&from, &fromLength);
+	if (bytes <= 0) {
+		return (int)bytes;
+	}
 	*fromAddress = ntohl(from.sin_addr.s_addr);
 	*fromPort = ntohs(from.sin_port);
-	return static_cast<int>(bytes);
+	return (int)bytes;
 #else
 	return 0;
 #endif
