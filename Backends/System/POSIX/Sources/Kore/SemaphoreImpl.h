f0cdc2fa73a90a834b19442c258a408577c55cd7
Robert Konrad <git2016@robdangero.us>
2020-11-13 05:33:22 +0100

    Use a non-deprecated semaphore implementation for macOS/iOS

--- Backends/System/POSIX/Sources/Kore/SemaphoreImpl.h
+++ Backends/System/POSIX/Sources/Kore/SemaphoreImpl.h
@@ -1,13 +1,21 @@
 #pragma once
 
+#ifdef __APPLE__
+#include <dispatch/dispatch.h>
+#else
 #include <semaphore.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct {
+#ifdef __APPLE__
+	dispatch_semaphore_t semaphore;
+#else
 	sem_t semaphore;
+#endif
 } kinc_semaphore_impl_t;
 
 #ifdef __cplusplus
