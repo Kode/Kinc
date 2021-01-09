def9657581473336ee875ee3fc09cbddb35eba11
Robert Konrad <git2016@robdangero.us>
2021-01-09 20:59:20 +0100

    Start implementing D3D12 compute support

--- Backends/Graphics5/Direct3D12/Sources/Kore/ComputeImpl.h
+++ Backends/Graphics5/Direct3D12/Sources/Kore/ComputeImpl.h
@@ -1,19 +1,42 @@
 #pragma once
 
+#include <Kore/ShaderHash.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+struct ID3D12Buffer;
+struct ID3D12PipelineState;
+
 typedef struct {
-	int a;
+	uint32_t offset;
+	uint32_t size;
+	uint8_t columns;
+	uint8_t rows;
 } kinc_compute_constant_location_impl_t;
 
 typedef struct {
-	int a;
+	int unit;
 } kinc_compute_texture_unit_impl_t;
 
 typedef struct {
-	int a;
+	uint32_t hash;
+	uint32_t offset;
+	uint32_t size;
+	uint8_t columns;
+	uint8_t rows;
+} kinc_compute_internal_shader_constant_t;
+
+typedef struct {
+	kinc_compute_internal_shader_constant_t constants[64];
+	int constantsSize;
+	kinc_internal_hash_index_t attributes[64];
+	kinc_internal_hash_index_t textures[64];
+	uint8_t *data;
+	int length;
+	struct ID3D12Buffer *constantBuffer;
+	struct ID3D12PipelineState *pso;
 } kinc_compute_shader_impl_t;
 
 #ifdef __cplusplus
