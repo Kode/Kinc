4da80313a9e13307ddf9f51b84d540c7c6455e12
Robert Konrad <git2016@robdangero.us>
2021-01-23 17:41:00 +0100

    Reimplement kinc_http_request for Apples to get rid of deprecated API calls

--- Backends/System/Apple/Sources/Kore/Http.mm
+++ Backends/System/Apple/Sources/Kore/Http.mm
@@ -4,58 +4,6 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Connection : NSObject <NSURLConnectionDelegate> {
-	NSMutableData* responseData;
-	kinc_http_callback_t callback;
-	void* data;
-	int statusCode;
-}
-
-@end
-
-@implementation Connection
-
-- (id)initWithCallback:(kinc_http_callback_t)aCallback andData:(void*)someData {
-	if (self = [super init]) {
-		callback = aCallback;
-		data = someData;
-		statusCode = 0;
-		return self;
-	}
-	else {
-		return nil;
-	}
-}
-
-- (void)connection:(NSURLConnection*)connection didReceiveResponse:(NSURLResponse*)response {
-	responseData = [[NSMutableData alloc] init];
-	NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
-	statusCode = (int)[httpResponse statusCode];
-}
-
-- (void)connection:(NSURLConnection*)connection didReceiveData:(NSData*)moreData {
-	[responseData appendData:moreData];
-}
-
-- (NSCachedURLResponse*)connection:(NSURLConnection*)connection willCacheResponse:(NSCachedURLResponse*)cachedResponse {
-	return nil;
-}
-
-- (void)connectionDidFinishLoading:(NSURLConnection*)connection {
-	[responseData appendBytes:"\0" length:1];
-	// printf("Got %s\n\n", (const char*)[responseData bytes]);
-	callback(0, statusCode, (const char*)[responseData bytes], data);
-}
-
-- (void)connection:(NSURLConnection*)connection didFailWithError:(NSError*)error {
-	[responseData appendBytes:"\0" length:1];
-	callback(1, statusCode, 0, data);
-}
-
-@end
-
-using namespace Kore;
-
 void kinc_http_request(const char* url, const char* path, const char* data, int port, bool secure, int method, const char* header, kinc_http_callback_t callback,
                        void* callbackdata) {
 	NSString* urlstring = secure ? @"https://" : @"http://";
@@ -66,30 +14,41 @@ void kinc_http_request(const char* url, const char* path, const char* data, int 
 	urlstring = [urlstring stringByAppendingString:[NSString stringWithUTF8String:path]];
 
 	NSURL* aUrl = [NSURL URLWithString:urlstring];
-	NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:aUrl cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
-	[request addValue:@"application/json" forHTTPHeaderField:@"Content-type"];
 
-	switch (method) {
-	case KINC_HTTP_GET:
-		[request setHTTPMethod:@"GET"];
-		break;
-	case KINC_HTTP_POST:
-		[request setHTTPMethod:@"POST"];
-		break;
-	case KINC_HTTP_PUT:
-		[request setHTTPMethod:@"PUT"];
-		break;
-	case KINC_HTTP_DELETE:
-		[request setHTTPMethod:@"DELETE"];
-		break;
-	}
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    sessionConfiguration.HTTPAdditionalHeaders = @{@"Content-Type"  : @"application/json"};
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:aUrl];
+    if (data != 0) {
+        // printf("Sending %s\n\n", data);
+        NSString* datastring = [NSString stringWithUTF8String:data];
+        request.HTTPBody = [datastring dataUsingEncoding:NSUTF8StringEncoding];
+    }
 
-	if (data != 0) {
-		// printf("Sending %s\n\n", data);
-		NSString* datastring = [NSString stringWithUTF8String:data];
-		[request setHTTPBody:[datastring dataUsingEncoding:NSUTF8StringEncoding]];
-	}
-
-	Connection* connection = [[Connection alloc] initWithCallback:callback andData:callbackdata];
-	[[NSURLConnection alloc] initWithRequest:request delegate:connection];
+    switch (method) {
+    case KINC_HTTP_GET:
+        request.HTTPMethod = @"GET";
+        break;
+    case KINC_HTTP_POST:
+        request.HTTPMethod = @"POST";
+        break;
+    case KINC_HTTP_PUT:
+        request.HTTPMethod = @"PUT";
+        break;
+    case KINC_HTTP_DELETE:
+        request.HTTPMethod = @"DELETE";
+        break;
+    }
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
+        int statusCode = (int)[httpResponse statusCode];
+        
+        NSMutableData* responseData = [[NSMutableData alloc] init];
+        [responseData appendData:data];
+        [responseData appendBytes:"\0" length:1];
+        
+        callback(error == nil ? 0 : 1, statusCode, (const char*)[responseData bytes], callbackdata);
+    }];
+    [dataTask resume];
 }
