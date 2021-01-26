04180a4145e7fc1b5d026116dcca45ffe8d40fc7
Robert Konrad <git2016@robdangero.us>
2021-01-26 05:44:26 +0100

    Fix another new Xcode warning

--- Sources/kinc/threads/thread.h
+++ Sources/kinc/threads/thread.h
@@ -10,8 +10,8 @@ typedef struct {
 	kinc_thread_impl_t impl;
 } kinc_thread_t;
 
-KINC_FUNC void kinc_threads_init();
-KINC_FUNC void kinc_threads_quit();
+KINC_FUNC void kinc_threads_init(void);
+KINC_FUNC void kinc_threads_quit(void);
 
 KINC_FUNC void kinc_thread_init(kinc_thread_t *thread, void (*func)(void *param), void *param);
 KINC_FUNC void kinc_thread_wait_and_destroy(kinc_thread_t *thread);
