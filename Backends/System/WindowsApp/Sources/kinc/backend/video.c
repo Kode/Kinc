3f834d47994641566aeba343edf26cb41bb94ce5
Robert Konrad <git2016@robdangero.us>
2021-02-19 23:57:46 +0100

    Setup video-sound-stream code for Windows apps

--- Backends/System/WindowsApp/Sources/kinc/backend/video.c
+++ Backends/System/WindowsApp/Sources/kinc/backend/video.c
@@ -41,3 +41,17 @@ bool kinc_video_paused(kinc_video_t *video) {
 }
 
 void kinc_video_update(kinc_video_t *video, double time) {}
+
+void kinc_video_sound_stream_impl_init(kinc_internal_video_sound_stream_t *stream, int channel_count, int frequency) {}
+
+void kinc_video_sound_stream_impl_destroy(kinc_internal_video_sound_stream_t *stream) {}
+
+void kinc_video_sound_stream_impl_insert_data(kinc_internal_video_sound_stream_t *stream, float *data, int sample_count) {}
+
+float kinc_video_sound_stream_impl_next_sample(kinc_internal_video_sound_stream_t *stream) {
+	return 0.0f;
+}
+
+bool kinc_video_sound_stream_impl_ended(kinc_internal_video_sound_stream_t *stream) {
+	return true;
+}
