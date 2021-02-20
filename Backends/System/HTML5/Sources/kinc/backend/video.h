e450caaad969ac46136060bdd33488e221d2435e
Robert Konrad <git2016@robdangero.us>
2021-02-20 15:57:35 +0100

    Copy in the new video-sound-stream things into the Linux and HTML5 backends

--- Backends/System/HTML5/Sources/kinc/backend/video.h
+++ Backends/System/HTML5/Sources/kinc/backend/video.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <kinc/graphics4/texture.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -9,6 +7,20 @@ extern "C" {
 typedef struct {
 	int nothing;
 } kinc_video_impl_t;
+
+typedef struct kinc_internal_video_sound_stream {
+	int nothing;
+} kinc_internal_video_sound_stream_t;
+
+void kinc_video_sound_stream_impl_init(kinc_internal_video_sound_stream_t *stream, int channel_count, int frequency);
+
+void kinc_video_sound_stream_impl_destroy(kinc_internal_video_sound_stream_t *stream);
+
+void kinc_video_sound_stream_impl_insert_data(kinc_internal_video_sound_stream_t *stream, float *data, int sample_count);
+
+float kinc_video_sound_stream_impl_next_sample(kinc_internal_video_sound_stream_t *stream);
+
+bool kinc_video_sound_stream_impl_ended(kinc_internal_video_sound_stream_t *stream);
 
 #ifdef __cplusplus
 }
