a8879841059a3005930676b241ee4cd38438567a
Robert Konrad <git2016@robdangero.us>
2021-04-25 15:44:34 +0200

    Add a few more Kinc matrix functions and use the same memory layout that Kore matrices use

--- Sources/kinc/math/matrix.h
+++ Sources/kinc/math/matrix.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "vector.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,9 +14,12 @@ KINC_FUNC float kinc_matrix3x3_get(kinc_matrix3x3_t *matrix, int x, int y);
 KINC_FUNC void kinc_matrix3x3_set(kinc_matrix3x3_t *matrix, int x, int y, float value);
 KINC_FUNC void kinc_matrix3x3_transpose(kinc_matrix3x3_t *matrix);
 KINC_FUNC kinc_matrix3x3_t kinc_matrix3x3_identity(void);
-KINC_FUNC kinc_matrix3x3_t kinc_matrix3x_rotation_x(float alpha);
-KINC_FUNC kinc_matrix3x3_t kinc_matrix3x_rotation_y(float alpha);
-KINC_FUNC kinc_matrix3x3_t kinc_matrix3x_rotation_z(float alpha);
+KINC_FUNC kinc_matrix3x3_t kinc_matrix3x3_rotation_x(float alpha);
+KINC_FUNC kinc_matrix3x3_t kinc_matrix3x3_rotation_y(float alpha);
+KINC_FUNC kinc_matrix3x3_t kinc_matrix3x3_rotation_z(float alpha);
+KINC_FUNC kinc_matrix3x3_t kinc_matrix3x3_translation(float x, float y);
+KINC_FUNC kinc_matrix3x3_t kinc_matrix3x3_multiply(kinc_matrix3x3_t *a, kinc_matrix3x3_t *b);
+KINC_FUNC kinc_vector3_t kinc_matrix3x3_multiply_vector(kinc_matrix3x3_t *a, kinc_vector3_t b);
 
 typedef struct kinc_matrix4x4 {
 	float m[4 * 4];
