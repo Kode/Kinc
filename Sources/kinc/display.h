6e49be970f5885b39e4513d46c1983bf4b283074
Robert Konrad <git2016@robdangero.us>
2021-05-16 02:32:51 +0200

    Add more comments

--- Sources/kinc/display.h
+++ Sources/kinc/display.h
@@ -23,18 +23,18 @@ typedef struct {
 } kinc_display_mode_t;
 
 /// <summary>
-/// Allows to retrieve display values prior to the kinc_init call
+/// Allows to retrieve display values prior to the kinc_init call.
 /// </summary>
 KINC_FUNC void kinc_display_init(void);
 
 /// <summary>
-/// Retrieves the index of the primary display
+/// Retrieves the index of the primary display.
 /// </summary>
 /// <returns>The index of the primary display</returns>
 KINC_FUNC int kinc_primary_display(void);
 
 /// <summary>
-/// Retrieves the number of displays connected to the system
+/// Retrieves the number of displays connected to the system.
 /// </summary>
 /// <remarks>
 /// All indices from 0 to kinc_count_displays() - 1 are legal display indices.
@@ -43,7 +43,7 @@ KINC_FUNC int kinc_primary_display(void);
 KINC_FUNC int kinc_count_displays(void);
 
 /// <summary>
-/// Checks whether the display index points to an available display
+/// Checks whether the display index points to an available display.
 /// </summary>
 /// <param name="display_index">Index of the display to check</param>
 /// <returns>
@@ -53,28 +53,28 @@ KINC_FUNC int kinc_count_displays(void);
 KINC_FUNC bool kinc_display_available(int display_index);
 
 /// <summary>
-/// Retrieves the system name of a display
+/// Retrieves the system name of a display.
 /// </summary>
 /// <param name="display_index">Index of the display to retrieve the name from</param>
 /// <returns>The system name of the display</returns>
 KINC_FUNC const char *kinc_display_name(int display_index);
 
 /// <summary>
-/// Retrieves the current mode of a display
+/// Retrieves the current mode of a display.
 /// </summary>
 /// <param name="display_index">Index of the display to retrieve the mode from</param>
 /// <returns>The current display mode</returns>
 KINC_FUNC kinc_display_mode_t kinc_display_current_mode(int display_index);
 
 /// <summary>
-/// Retrieves the number of available modes of a display
+/// Retrieves the number of available modes of a display.
 /// </summary>
 /// <param name="display_index">Index of the display to retrieve the modes count from</param>
 /// <returns>The number of available modes of the display</returns>
 KINC_FUNC int kinc_display_count_available_modes(int display_index);
 
 /// <summary>
-/// Retrieves a specific mode of a display
+/// Retrieves a specific mode of a display.
 /// </summary>
 /// <param name="display_index">Index of the display to retrieve the mode from</param>
 /// <param name="mode_index">Index of the mode to retrieve</param>
--- Sources/kinc/video.h
+++ Sources/kinc/video.h
@@ -5,6 +5,11 @@
 #include <kinc/backend/video.h>
 #include <kinc/graphics4/texture.h>
 
+/*! \file video.h
+    \brief Hardware-assisted video decoding support. Actually supported
+    formats vary per target-system.
+*/
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,31 +18,84 @@ typedef struct {
 	kinc_video_impl_t impl;
 } kinc_video_t;
 
-void kinc_video_init(kinc_video_t *video, const char *filename);
+/// <summary>
+/// Returns the list of video formats which are supported on the current system.
+/// </summary>
+/// <returns>A NULL-terminated list of strings representing the supported video-formats</returns>
+KINC_FUNC const char **kinc_video_formats(void);
 
-void kinc_video_destroy(kinc_video_t *video);
+/// <summary>
+/// Prepares a video object based on an encoded video-file.
+/// </summary>
+KINC_FUNC void kinc_video_init(kinc_video_t *video, const char *filename);
 
-void kinc_video_play(kinc_video_t *video);
+/// <summary>
+/// Destroys a video object.
+/// </summary>
+KINC_FUNC void kinc_video_destroy(kinc_video_t *video);
 
-void kinc_video_pause(kinc_video_t *video);
+/// <summary>
+/// Starts playing a video.
+/// </summary>
+KINC_FUNC void kinc_video_play(kinc_video_t *video);
 
-void kinc_video_stop(kinc_video_t *video);
+/// <summary>
+/// Pauses a video.
+/// </summary>
+KINC_FUNC void kinc_video_pause(kinc_video_t *video);
 
-int kinc_video_width(kinc_video_t *video);
+/// <summary>
+/// Stops a video which is equivalent to pausing it and setting the position to 0.
+/// </summary>
+KINC_FUNC void kinc_video_stop(kinc_video_t *video);
 
-int kinc_video_height(kinc_video_t *video);
+/// <summary>
+/// Gets the width of the video in pixels.
+/// </summary>
+/// <returns>The width of the video in pixels</returns>
+KINC_FUNC int kinc_video_width(kinc_video_t *video);
 
-kinc_g4_texture_t *kinc_video_current_image(kinc_video_t *video);
+/// <summary>
+/// Gets the height of the video in pixels.
+/// </summary>
+/// <returns>The height of the video in pixels</returns>
+KINC_FUNC int kinc_video_height(kinc_video_t *video);
 
-double kinc_video_duration(kinc_video_t *video);
+/// <summary>
+/// Gets the current image of a playing video which can be rendered using any of Kinc's graphics APIs.
+/// </summary>
+/// <returns>The current image of a playing video</returns>
+KINC_FUNC kinc_g4_texture_t *kinc_video_current_image(kinc_video_t *video);
 
-double kinc_video_position(kinc_video_t *video);
+/// <summary>
+/// Gets the duration of the video in seconds.
+/// </summary>
+/// <returns>The duration of the video in seconds</returns>
+KINC_FUNC double kinc_video_duration(kinc_video_t *video);
 
-bool kinc_video_finished(kinc_video_t *video);
+/// <summary>
+/// Returns the current playback-position of the video in seconds.
+/// </summary>
+/// <returns>The current playback-position in seconds</returns>
+KINC_FUNC double kinc_video_position(kinc_video_t *video);
 
-bool kinc_video_paused(kinc_video_t *video);
+/// <summary>
+/// Returns whether the video reached its end.
+/// </summary>
+/// <returns>the end-state of the video</returns>
+KINC_FUNC bool kinc_video_finished(kinc_video_t *video);
 
-void kinc_video_update(kinc_video_t *video, double time);
+/// <summary>
+/// Returns whether the video is currently paused.
+/// </summary>
+/// <returns>The current pause state of the video</returns>
+KINC_FUNC bool kinc_video_paused(kinc_video_t *video);
+
+/// <summary>
+/// Call this every frame to update the video. This is not required on all targets but where it's not required the function just does nothing - so please call
+/// it.
+/// </summary>
+KINC_FUNC void kinc_video_update(kinc_video_t *video, double time);
 
 #ifdef __cplusplus
 }
