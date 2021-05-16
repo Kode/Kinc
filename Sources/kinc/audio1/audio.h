e8948f993a9861de0274f0c2b9b05abbe2bd6bbd
Robert Konrad <git2016@robdangero.us>
2021-05-16 15:30:19 +0200

    Comment Audio1

--- Sources/kinc/audio1/audio.h
+++ Sources/kinc/audio1/audio.h
@@ -7,6 +7,11 @@
 
 #include <stdbool.h>
 
+/*! \file audio.h
+    \brief Audio1 is a high-level audio-API that lets you directly play audio-files. Depending on the target-system it either sits directly on a high-level
+   system audio-API or is implemented based on Audio2.
+*/
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -31,11 +36,37 @@ typedef struct {
 	int position;
 } kinc_internal_video_channel_t;
 
+/// <summary>
+/// Initialize the Audio1-API.
+/// </summary>
 KINC_FUNC void kinc_a1_init(void);
-KINC_FUNC kinc_a1_channel_t *kinc_a1_play_sound(kinc_a1_sound_t *sound, bool loop, float pitch,
-                                                bool unique); //(kinc_a1_sound_t *sound, bool loop = false, float pitch = 1.0f, bool unique = false);
+
+/// <summary>
+/// Plays a sound immediately.
+/// </summary>
+/// <param name="sound">The sound to play</param>
+/// <param name="loop">Whether or not to automatically loop the sound</param>
+/// <param name="pitch">Changes the pitch by providing a value that's not 1.0f</param>
+/// <param name="unique">Makes sure that a sound is not played more than once at the same time</param>
+/// <returns>A channel object that can be used to control the playing sound</returns>
+KINC_FUNC kinc_a1_channel_t *kinc_a1_play_sound(kinc_a1_sound_t *sound, bool loop, float pitch, bool unique);
+
+/// <summary>
+/// Stops the sound from playing.
+/// </summary>
+/// <param name="sound">The sound to stop</param>
 KINC_FUNC void kinc_a1_stop_sound(kinc_a1_sound_t *sound);
+
+/// <summary>
+/// Starts playing a sound-stream.
+/// </summary>
+/// <param name="stream">The stream to play</param>
 KINC_FUNC void kinc_a1_play_sound_stream(kinc_a1_sound_stream_t *stream);
+
+/// <summary>
+/// Stops a sound-stream from playing.
+/// </summary>
+/// <param name="stream">The stream to stop.</param>
 KINC_FUNC void kinc_a1_stop_sound_stream(kinc_a1_sound_stream_t *stream);
 
 void kinc_internal_play_video_sound_stream(struct kinc_internal_video_sound_stream *stream);
--- Sources/kinc/audio1/sound.h
+++ Sources/kinc/audio1/sound.h
@@ -6,6 +6,10 @@
 
 #include <stdint.h>
 
+/*! \file sound.h
+    \brief Sounds are pre-decoded on load and therefore primarily useful for playing sound-effects.
+*/
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -20,9 +24,31 @@ typedef struct {
 	bool in_use;
 } kinc_a1_sound_t;
 
+/// <summary>
+/// Create a sound from a wav file.
+/// </summary>
+/// <param name="filename">Path to a wav file</param>
+/// <returns>The newly created sound</returns>
 KINC_FUNC kinc_a1_sound_t *kinc_a1_sound_create(const char *filename);
+
+/// <summary>
+/// Destroy a sound.
+/// </summary>
+/// <param name="sound">The sound to destroy.</param>
 KINC_FUNC void kinc_a1_sound_destroy(kinc_a1_sound_t *sound);
+
+/// <summary>
+/// Gets the current volume-multiplicator that's used when playing the sound.
+/// </summary>
+/// <param name="sound">The sound to query</param>
+/// <returns>The volume-multiplicator</returns>
 KINC_FUNC float kinc_a1_sound_volume(kinc_a1_sound_t *sound);
+
+/// <summary>
+/// Sets the volume-multiplicator that's used when playing the sound.
+/// </summary>
+/// <param name="sound">The sound to modify</param>
+/// <param name="value">The volume-multiplicator to set</param>
 KINC_FUNC void kinc_a1_sound_set_volume(kinc_a1_sound_t *sound, float value);
 
 #ifdef __cplusplus
