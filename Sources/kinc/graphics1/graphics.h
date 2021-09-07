9c17f660701ec28cc1fccd654de06aa6cce4eb34
Robert Konrad <git2016@robdangero.us>
2021-09-07 20:58:49 +0200

    Try to fix the G1 documentation

--- Sources/kinc/graphics1/graphics.h
+++ Sources/kinc/graphics1/graphics.h
@@ -34,15 +34,7 @@ KINC_FUNC void kinc_g1_end(void);
 extern uint32_t *kinc_internal_g1_image;
 extern int kinc_internal_g1_w, kinc_internal_g1_h, kinc_internal_g1_tex_width;
 	
-#if defined(KINC_DYNAMIC_COMPILE) || defined(KINC_DYNAMIC)
-
-KINC_FUNC void kinc_g1_set_pixel(int x, int y, float red, float green, float blue);
-KINC_FUNC int kinc_g1_width(void);
-KINC_FUNC int kinc_g1_height(void);
-
-#else
-
-// implementation moved to the header to allow easy inlining
+#if defined(KINC_DYNAMIC_COMPILE) || defined(KINC_DYNAMIC) || defined(KINC_DOCS)
 
 /// <summary>
 /// Sets a single pixel to a color.
@@ -52,6 +44,24 @@ KINC_FUNC int kinc_g1_height(void);
 /// <param name="red">The red-component between 0 and 1</param>
 /// <param name="green">The green-component between 0 and 1</param>
 /// <param name="blue">The blue-component between 0 and 1</param>
+KINC_FUNC void kinc_g1_set_pixel(int x, int y, float red, float green, float blue);
+
+/// <summary>
+/// Returns the width used by G1.
+/// </summary>
+/// <returns>The width</returns>
+KINC_FUNC int kinc_g1_width(void);
+
+/// <summary>
+/// Returns the height used by G1.
+/// </summary>
+/// <returns>The height</returns>
+KINC_FUNC int kinc_g1_height(void);
+
+#else
+
+// implementation moved to the header to allow easy inlining
+
 static inline void kinc_g1_set_pixel(int x, int y, float red, float green, float blue) {
 	assert(x >= 0 && x < kinc_internal_g1_w && y >= 0 && y < kinc_internal_g1_h);
 	int r = (int)(red * 255);
@@ -60,18 +70,10 @@ static inline void kinc_g1_set_pixel(int x, int y, float red, float green, float
 	kinc_internal_g1_image[y * kinc_internal_g1_tex_width + x] = 0xff << 24 | b << 16 | g << 8 | r;
 }
 
-/// <summary>
-/// Returns the width used by G1.
-/// </summary>
-/// <returns>The width</returns>
 static inline int kinc_g1_width() {
 	return kinc_internal_g1_w;
 }
 
-/// <summary>
-/// Returns the height used by G1.
-/// </summary>
-/// <returns>The height</returns>
 static inline int kinc_g1_height() {
 	return kinc_internal_g1_h;
 }
