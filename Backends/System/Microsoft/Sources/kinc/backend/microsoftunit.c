ddc4680b1972a97d2c6b86baad90774387f299a4
Robert Konrad <git2016@robdangero.us>
2021-09-21 18:21:50 +0200

    Put in a minimal Windows.h include for reference

--- Backends/System/Microsoft/Sources/kinc/backend/microsoftunit.c
+++ Backends/System/Microsoft/Sources/kinc/backend/microsoftunit.c
@@ -1,3 +1,50 @@
+#define _WIN32_WINNT 0x0501 // Windows XP
+
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCOMM
+#define NOCTLMGR
+#define NODEFERWINDOWPOS
+#define NODRAWTEXT
+#define NOGDI
+#define NOGDICAPMASKS
+#define NOHELP
+#define NOICONS
+#define NOKANJI
+#define NOKEYSTATES
+#define NOMB
+#define NOMCX
+#define NOMEMMGR
+#define NOMENUS
+#define NOMETAFILE
+#define NOMINMAX
+#define NOMSG
+#define NONLS
+#define NOOPENFILE
+#define NOPROFILER
+#define NORASTEROPS
+#define NOSCROLL
+#define NOSERVICE
+#define NOSHOWWINDOW
+#define NOSOUND
+#define NOSYSCOMMANDS
+#define NOSYSMETRICS
+#define NOTEXTMETRIC
+#define NOUSER
+#define NOVIRTUALKEYCODES
+#define NOWH
+#define NOWINMESSAGES
+#define NOWINOFFSETS
+#define NOWINSTYLES
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
+#include <assert.h>
+#include <intrin.h>
+#include <stdio.h>
+
 #include "SystemMicrosoft.c.h"
 #include "event.c.h"
 #include "fiber.c.h"
