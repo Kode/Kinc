9c71357efc52405fe963641204d32bdcb8570c2b
Robert Konrad <git2016@robdangero.us>
2021-12-21 18:56:24 +0100

    Remove a superfluous assert

--- Backends/Graphics5/Vulkan/Sources/kinc/backend/graphics5/indexbuffer.c.h
+++ Backends/Graphics5/Vulkan/Sources/kinc/backend/graphics5/indexbuffer.c.h
@@ -40,7 +40,6 @@ void kinc_g5_index_buffer_init(kinc_g5_index_buffer_t *buffer, int indexCount, k
 
 	VkMemoryRequirements mem_reqs = {0};
 	vkGetBufferMemoryRequirements(device, buffer->impl.buf, &mem_reqs);
-	assert(!err);
 
 	buffer->impl.mem_alloc.allocationSize = mem_reqs.size;
 	bool pass = memory_type_from_properties(mem_reqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, &buffer->impl.mem_alloc.memoryTypeIndex);
