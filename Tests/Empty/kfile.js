a094b043f029693a7fcf9bc9d37264590a667f61
Robert Konrad <git2016@robdangero.us>
2022-01-14 14:38:09 +0100

    Fix the kfile for the test

--- Tests/Empty/kfile.js
+++ Tests/Empty/kfile.js
@@ -3,9 +3,10 @@ let project = new Project('Empty');
 project.cpp = true;
 
 await project.addProject('../../');
-project.flatten();
 
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
+
+project.flatten();
 
 resolve(project);
--- Tests/Shader/kfile.js
+++ Tests/Shader/kfile.js
@@ -1,6 +1,6 @@
 const project = new Project('ShaderTest');
 
-await project.addProject('Kinc');
+await project.addProject('../../');
 
 project.addFile('Sources/**');
 project.addFile('Shaders/**');
