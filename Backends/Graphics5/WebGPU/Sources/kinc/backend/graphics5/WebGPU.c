d7bbf789f0a589bf0ab2f80c62694b2f81553bf0
Robert Konrad <git2016@robdangero.us>
2022-03-28 23:47:15 +0200

    Move more functions to the command-list in WebGPU

--- Backends/Graphics5/WebGPU/Sources/kinc/backend/graphics5/WebGPU.c
+++ Backends/Graphics5/WebGPU/Sources/kinc/backend/graphics5/WebGPU.c
@@ -57,8 +57,6 @@ void kinc_g5_draw_indexed_vertices_instanced_from_to(int instanceCount, int star
 
 void kinc_g5_draw_indexed_vertices_instanced_from_to_from(int instanceCount, int start, int count, int vertex_offset) {}
 
-void kinc_g5_set_texture_addressing(kinc_g5_texture_unit_t unit, kinc_g5_texture_direction_t dir, kinc_g5_texture_addressing_t addressing) {}
-
 void kinc_g5_begin(kinc_g5_render_target_t *renderTarget, int window) {}
 
 void kinc_g5_end(int window) {}
@@ -69,37 +67,9 @@ bool kinc_g5_swap_buffers() {
 
 void kinc_g5_flush() {}
 
-void kinc_g5_set_texture_magnification_filter(kinc_g5_texture_unit_t texunit, kinc_g5_texture_filter_t filter) {}
-
-void kinc_g5_set_texture_minification_filter(kinc_g5_texture_unit_t texunit, kinc_g5_texture_filter_t filter) {}
-
-void kinc_g5_set_texture_mipmap_filter(kinc_g5_texture_unit_t texunit, kinc_g5_mipmap_filter_t filter) {}
-
 bool kinc_g5_render_targets_inverted_y() {
 	return false;
 }
 bool kinc_g5_non_pow2_textures_qupported() {
 	return true;
 }
-
-void kinc_g5_set_render_target_face(kinc_g5_render_target_t *texture, int face) {}
-
-void kinc_g5_set_texture(kinc_g5_texture_unit_t unit, kinc_g5_texture_t *texture) {
-
-}
-
-void kinc_g5_set_image_texture(kinc_g5_texture_unit_t unit, kinc_g5_texture_t *texture) {}
-
-bool kinc_g5_init_occlusion_query(unsigned *occlusionQuery) {
-	return false;
-}
-
-void kinc_g5_delete_occlusion_query(unsigned occlusionQuery) {}
-
-void kinc_g5_render_occlusion_query(unsigned occlusionQuery, int triangles) {}
-
-bool kinc_g5_are_query_results_available(unsigned occlusionQuery) {
-	return false;
-}
-
-void kinc_g5_get_query_result(unsigned occlusionQuery, unsigned *pixelCount) {}
