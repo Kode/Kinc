483d8065802019ae8110ae379c9447cce75ca465
tcdude <tc@tizilogic.com>
2022-04-11 23:56:47 +0200

    Add int8x16

--- Sources/kinc/simd/types.h
+++ Sources/kinc/simd/types.h
@@ -13,16 +13,19 @@ extern "C" {
 #if defined(__SSE__) || _M_IX86_FP == 2 || _M_IX86_FP == 1 || (defined(KORE_WINDOWS) && !defined(__aarch64__)) ||                                              \
     (defined(KORE_WINDOWSAPP) && !defined(__aarch64__)) || (defined(KORE_MACOS) && __x86_64)
 
-#define KINC_SSE
-#if !defined(__SSE2__) && _M_IX86_FP < 2
-#define KINC_NOSSE2
-#endif
-
 #include <xmmintrin.h>
 
 typedef __m128 kinc_float32x4_t;
 typedef __m128 kinc_float32x4_mask_t;
 
+#define KINC_SSE
+#if defined(__SSE2__) || _M_IX86_FP == 2
+#define KINC_SSE2
+typedef __m128i kinc_int8x16_t;
+typedef __m128i kinc_int8x16_mask_t;
+typedef __m128i kinc_uint8x16_t;
+typedef __m128i kinc_uint8x16_mask_t;
+#endif
 
 #elif defined(KORE_IOS) || defined(KORE_SWITCH) || defined(__aarch64__) || defined(KORE_NEON)
 
@@ -31,6 +34,10 @@ typedef __m128 kinc_float32x4_mask_t;
 
 typedef float32x4_t kinc_float32x4_t;
 typedef uint32x4_t kinc_float32x4_mask_t;
+typedef int8x16_t kinc_int8x16_t;
+typedef uint8x16_t kinc_int8x16_mask_t;
+typedef uint8x16_t kinc_uint8x16_t;
+typedef uint8x16_t kinc_uint8x16_mask_t;
 
 #else
 
@@ -43,6 +50,23 @@ typedef struct kinc_float32x4 {
 typedef struct kinc_float32x4_mask {
 	int values[4];
 } kinc_float32x4_mask_t;
+
+typedef struct kinc_int8x16 {
+	int8_t values[16];
+} kinc_int8x16_t;
+
+typedef struct kinc_int8x16_mask {
+	uint8_t values[16];
+} kinc_int8x16_mask_t;
+
+typedef struct kinc_uint8x16 {
+	uint8_t values[16];
+} kinc_uint8x16_t;
+
+typedef struct kinc_uint8x16_mask {
+	uint8_t values[16];
+} kinc_uint8x16_mask_t;
+
 
 #endif
 
