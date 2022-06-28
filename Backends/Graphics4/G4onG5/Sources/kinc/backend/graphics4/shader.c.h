faa3a6b7ffbe8141e3e043c4f6239c10dd5b60fb
Robert Konrad <git2016@robdangero.us>
2022-06-29 00:36:04 +0200

    Support kinc_g4_shader_init_from_source on G5-backends

--- Backends/Graphics4/G4onG5/Sources/kinc/backend/graphics4/shader.c.h
+++ Backends/Graphics4/G4onG5/Sources/kinc/backend/graphics4/shader.c.h
@@ -4,8 +4,45 @@ void kinc_g4_shader_init(kinc_g4_shader_t *shader, void *_data, size_t length, k
 	kinc_g5_shader_init(&shader->impl._shader, _data, length, (kinc_g5_shader_type_t)type);
 }
 
+#ifdef KRAFIX_LIBRARY
+#include <kinc/memory.h>
+extern int krafix_compile(const char *source, char *output, int *length, const char *targetlang, const char *system, const char *shadertype);
+#endif
+
 int kinc_g4_shader_init_from_source(kinc_g4_shader_t *shader, const char *source, kinc_g4_shader_type_t type) {
-	return 1;
+#ifdef KRAFIX_LIBRARY
+	char *output = kinc_allocate(1024 * 1024);
+	int length;
+
+#ifdef KINC_WINDOWS
+	const char *system = "windows";
+#elif defined(KINC_MACOS)
+	const char *system = "macos";
+#elif defined(KINC_LINUX)
+	const char *system = "linux";
+#elif defined(KINC_ANDROID)
+	const char *system = "android";
+#elif defined(KINC_IOS)
+	const char *system = "ios";
+#endif
+
+#ifdef KORE_VULKAN
+	const char *target = "spirv";
+#elif defined(KORE_METAL)
+	const char *target = "metal";
+#else
+	const char *target = "d3d11";
+#endif
+
+	int errors = krafix_compile(source, output, &length, target, system, type == KINC_G4_SHADER_TYPE_FRAGMENT ? "frag" : "vert");
+	if (errors > 0) {
+		return errors;
+	}
+	kinc_g4_shader_init(shader, output, length, type);
+	return 0;
+#else
+	return 0;
+#endif
 }
 
 void kinc_g4_shader_destroy(kinc_g4_shader_t *shader) {
