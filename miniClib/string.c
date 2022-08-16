94b4e0d4e8acfa504c4c40341fda421925694da1
Robert Konrad <git2016@robdangero.us>
2022-08-16 20:41:02 +0200

    Fix up string.h

--- miniClib/string.c
+++ miniClib/string.c
@@ -1,62 +1,8 @@
 #pragma once
 
-#include <kinc/global.h>
+#include "string.h"
 
-#include <stddef.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-KINC_FUNC size_t kinc_string_length(const char *str);
-
-KINC_FUNC char *kinc_string_copy(char *destination, const char *source);
-
-KINC_FUNC char *kinc_string_copy_limited(char *destination, const char *source, size_t num);
-
-KINC_FUNC char *kinc_string_append(char *destination, const char *source);
-
-KINC_FUNC char *kinc_string_find(char *str1, const char *str2);
-
-KINC_FUNC int kinc_string_compare(const char *str1, const char *str2);
-
-KINC_FUNC int kinc_string_compare_limited(const char *str1, const char *str2, size_t num);
-
-KINC_FUNC char *kinc_string_duplicate(const char *str);
-
-KINC_FUNC size_t kinc_wstring_length(const wchar_t *str);
-
-KINC_FUNC wchar_t *kinc_wstring_copy(wchar_t *destination, const wchar_t *source);
-
-KINC_FUNC wchar_t *kinc_wstring_copy_limited(wchar_t *destination, const wchar_t *source, size_t num);
-
-KINC_FUNC wchar_t *kinc_wstring_append(wchar_t *destination, const wchar_t *source);
-
-KINC_FUNC wchar_t *kinc_wstring_find(wchar_t *str1, const wchar_t *str2);
-
-KINC_FUNC int kinc_wstring_compare(const wchar_t *str1, const wchar_t *str2);
-
-KINC_FUNC int kinc_wstring_compare_limited(const wchar_t *str1, const wchar_t *str2, size_t num);
-
-#ifdef KINC_IMPLEMENTATION_ROOT
-#define KINC_IMPLEMENTATION
-#endif
-
-#ifdef KINC_IMPLEMENTATION
-
-#include <kinc/memory.h>
-
-#include <string.h>
-#include <wchar.h>
-
-#define STB_SPRINTF_IMPLEMENTATION
-#include <kinc/libs/stb_sprintf.h>
-
-#undef strcmp
-#undef strncmp
-
-size_t kinc_string_length(const char *str) {
-#ifdef KINC_NO_CLIB
+size_t strlen(const char *str) {
 	size_t size = 0;
 	while (true) {
 		if (str[size] == 0) {
@@ -65,13 +11,9 @@ size_t kinc_string_length(const char *str) {
 		++size;
 	}
 	return 0;
-#else
-	return strlen(str);
-#endif
 }
 
-char *kinc_string_copy(char *destination, const char *source) {
-#ifdef KINC_NO_CLIB
+char *strcpy(char *destination, const char *source) {
 	for (size_t i = 0;; ++i) {
 		destination[i] = source[i];
 		if (source[i] == 0) {
@@ -79,13 +21,9 @@ char *kinc_string_copy(char *destination, const char *source) {
 		}
 	}
 	return destination;
-#else
-	return strcpy(destination, source);
-#endif
 }
 
-char *kinc_string_copy_limited(char *destination, const char *source, size_t num) {
-#ifdef KINC_NO_CLIB
+char *strncpy(char *destination, const char *source, size_t num) {
 	for (size_t i = 0; i < num; ++i) {
 		destination[i] = source[i];
 		if (source[i] == 0) {
@@ -93,13 +31,9 @@ char *kinc_string_copy_limited(char *destination, const char *source, size_t num
 		}
 	}
 	return destination;
-#else
-	return strncpy(destination, source, num);
-#endif
 }
 
-char *kinc_string_append(char *destination, const char *source) {
-#ifdef KINC_NO_CLIB
+char *strcat(char *destination, const char *source) {
 	size_t di = 0;
 	while (destination[di] != 0) {
 		++di;
@@ -112,13 +46,9 @@ char *kinc_string_append(char *destination, const char *source) {
 		++di;
 	}
 	return destination;
-#else
-	return strcat(destination, source);
-#endif
 }
 
-char *kinc_string_find(char *str1, const char *str2) {
-#ifdef KINC_NO_CLIB
+char *strstr(char *str1, const char *str2) {
 	for (size_t i1 = 0;; ++i1) {
 		if (str1[i1] == 0) {
 			return NULL;
@@ -132,13 +62,9 @@ char *kinc_string_find(char *str1, const char *str2) {
 			}
 		}
 	}
-#else
-	return strstr(str1, str2);
-#endif
 }
 
-int kinc_string_compare(const char *str1, const char *str2) {
-#ifdef KINC_NO_CLIB
+int strcmp(const char *str1, const char *str2) {
 	for (size_t i = 0;; ++i) {
 		if (str1[i] != str2[i]) {
 			return str1[i] - str2[i];
@@ -147,13 +73,9 @@ int kinc_string_compare(const char *str1, const char *str2) {
 			return 0;
 		}
 	}
-#else
-	return strcmp(str1, str2);
-#endif
 }
 
-int kinc_string_compare_limited(const char *str1, const char *str2, size_t num) {
-#ifdef KINC_NO_CLIB
+int strncmp(const char *str1, const char *str2, size_t num) {
 	for (size_t i = 0; i < num; ++i) {
 		if (str1[i] != str2[i]) {
 			return str1[i] - str2[i];
@@ -163,19 +85,9 @@ int kinc_string_compare_limited(const char *str1, const char *str2, size_t num) 
 		}
 	}
 	return 0;
-#else
-	return strncmp(str1, str2, num);
-#endif
 }
 
-char *kinc_string_duplicate(const char *str) {
-	char *ret = kinc_allocate(kinc_string_length(str) + 1);
-	kinc_string_copy(ret, str);
-	return ret;
-}
-
-size_t kinc_wstring_length(const wchar_t *str) {
-#ifdef KINC_NO_CLIB
+size_t wcslen(const wchar_t *str) {
 	size_t size = 0;
 	while (true) {
 		if (str[size] == 0) {
@@ -184,13 +96,9 @@ size_t kinc_wstring_length(const wchar_t *str) {
 		++size;
 	}
 	return 0;
-#else
-	return wcslen(str);
-#endif
 }
 
-wchar_t *kinc_wstring_copy(wchar_t *destination, const wchar_t *source) {
-#ifdef KINC_NO_CLIB
+wchar_t *wcscpy(wchar_t *destination, const wchar_t *source) {
 	for (size_t i = 0;; ++i) {
 		destination[i] = source[i];
 		if (source[i] == 0) {
@@ -198,13 +106,9 @@ wchar_t *kinc_wstring_copy(wchar_t *destination, const wchar_t *source) {
 		}
 	}
 	return destination;
-#else
-	return wcscpy(destination, source);
-#endif
 }
 
-wchar_t *kinc_wstring_copy_limited(wchar_t *destination, const wchar_t *source, size_t num) {
-#ifdef KINC_NO_CLIB
+wchar_t *wcsncpy(wchar_t *destination, const wchar_t *source, size_t num) {
 	for (size_t i = 0; i < num; ++i) {
 		destination[i] = source[i];
 		if (source[i] == 0) {
@@ -212,13 +116,9 @@ wchar_t *kinc_wstring_copy_limited(wchar_t *destination, const wchar_t *source, 
 		}
 	}
 	return destination;
-#else
-	return wcsncpy(destination, source, num);
-#endif
 }
 
-wchar_t *kinc_wstring_append(wchar_t *destination, const wchar_t *source) {
-#ifdef KINC_NO_CLIB
+wchar_t *wcscat(wchar_t *destination, const wchar_t *source) {
 	size_t di = 0;
 	while (destination[di] != 0) {
 		++di;
@@ -231,13 +131,9 @@ wchar_t *kinc_wstring_append(wchar_t *destination, const wchar_t *source) {
 		++di;
 	}
 	return destination;
-#else
-	return wcscat(destination, source);
-#endif
 }
 
-wchar_t *kinc_wstring_find(wchar_t *str1, const wchar_t *str2) {
-#ifdef KINC_NO_CLIB
+wchar_t *wcsstr(wchar_t *str1, const wchar_t *str2) {
 	for (size_t i1 = 0;; ++i1) {
 		if (str1[i1] == 0) {
 			return NULL;
@@ -251,13 +147,9 @@ wchar_t *kinc_wstring_find(wchar_t *str1, const wchar_t *str2) {
 			}
 		}
 	}
-#else
-	return wcsstr(str1, str2);
-#endif
 }
 
-int kinc_wstring_compare(const wchar_t *str1, const wchar_t *str2) {
-#ifdef KINC_NO_CLIB
+int wcscmp(const wchar_t *str1, const wchar_t *str2) {
 	for (size_t i = 0;; ++i) {
 		if (str1[i] != str2[i]) {
 			return str1[i] - str2[i];
@@ -266,13 +158,9 @@ int kinc_wstring_compare(const wchar_t *str1, const wchar_t *str2) {
 			return 0;
 		}
 	}
-#else
-	return wcscmp(str1, str2);
-#endif
 }
 
-int kinc_wstring_compare_limited(const wchar_t *str1, const wchar_t *str2, size_t num) {
-#ifdef KINC_NO_CLIB
+int wcsncmp(const wchar_t *str1, const wchar_t *str2, size_t num) {
 	for (size_t i = 0; i < num; ++i) {
 		if (str1[i] != str2[i]) {
 			return str1[i] - str2[i];
@@ -282,13 +170,4 @@ int kinc_wstring_compare_limited(const wchar_t *str1, const wchar_t *str2, size_
 		}
 	}
 	return 0;
-#else
-	return wcsncmp(str1, str2, num);
-#endif
 }
-
-#endif
-
-#ifdef __cplusplus
-}
-#endif
--- miniClib/string.h
+++ miniClib/string.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+size_t strlen(const char *str);
+
+char *strcpy(char *destination, const char *source);
+
+char *strncpy(char *destination, const char *source, size_t num);
+
+char *strcat(char *destination, const char *source);
+
+char *strstr(char *str1, const char *str2);
+
+int strcmp(const char *str1, const char *str2);
+
+int strncmp(const char *str1, const char *str2, size_t num);
+
+size_t wcslen(const wchar_t *str);
+
+wchar_t *wcscpy(wchar_t *destination, const wchar_t *source);
+
+wchar_t *wcsncpy(wchar_t *destination, const wchar_t *source, size_t num);
+
+wchar_t *wcscat(wchar_t *destination, const wchar_t *source);
+
+wchar_t *wcsstr(wchar_t *str1, const wchar_t *str2);
+
+int wcscmp(const wchar_t *str1, const wchar_t *str2);
+
+int wcsncmp(const wchar_t *str1, const wchar_t *str2, size_t num);
+
+#ifdef __cplusplus
+}
+#endif
