07afd1fc838142e0146cf4be36dcaf24fd301420
Robert Konrad <git2016@robdangero.us>
2022-09-22 17:12:52 +0200

    Switch a clang-warning

--- Sources/kinc/audio1/a1unit.c
+++ Sources/kinc/audio1/a1unit.c
@@ -2,23 +2,23 @@
 
 #include <stdbool.h>
 
-typedef struct kinc_a1_channel {
+struct kinc_a1_channel {
 	kinc_a1_sound_t *sound;
 	float position;
 	bool loop;
 	volatile float volume;
 	float pitch;
-} kinc_a1_channel_t;
+};
 
-typedef struct kinc_a1_stream_channel {
+struct kinc_a1_stream_channel {
 	kinc_a1_sound_stream_t *stream;
 	int position;
-} kinc_a1_stream_channel_t;
+};
 
-typedef struct kinc_internal_video_channel {
+struct kinc_internal_video_channel {
 	struct kinc_internal_video_sound_stream *stream;
 	int position;
-} kinc_internal_video_channel_t;
+};
 
 #include "audio.c.h"
 #include "sound.c.h"
