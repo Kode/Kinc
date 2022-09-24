e06c56086b27a64df074aa55d32c95eb9ff3fd2f
Robert Konrad <git2016@robdangero.us>
2022-09-24 20:48:10 +0200

    Improve the file-check when dangerously checking the dlc

--- get_dlc_dangerously.bat
+++ get_dlc_dangerously.bat
@@ -1,5 +1,5 @@
 @pushd "%~dp0"
-@if exist Tools\windows_x64\kmake.exe (
+@if exist Tools\windows_x64\icon.png (
 @git submodule update --remote --merge Tools/windows_x64
 ) else (
 @git submodule update --init --remote Tools/windows_x64
