775ff26e4dde46f2953288be790db95af6638b67
Robert Konrad <git2016@robdangero.us>
2022-09-24 20:30:32 +0200

    Stay on the main-branches when dangerously getting the dlc

--- get_dlc_dangerously.bat
+++ get_dlc_dangerously.bat
@@ -1,3 +1,8 @@
 @pushd "%~dp0"
+@if exist Tools\windows_x64\kmake.exe (
+@git submodule update --remote --merge Tools/windows_x64
+) else (
 @git submodule update --init --remote Tools/windows_x64
+@git -C Tools/windows_x64 checkout main
+)
 @popd
