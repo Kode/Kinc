3746153a822f52aca7f2014d1c86cc789280003d
Apprentice-Alchemist <53486764+Apprentice-Alchemist@users.noreply.github.com>
2022-09-27 21:41:51 +0200

    Fix D3D9

--- Backends/Graphics4/Direct3D9/Sources/kinc/backend/graphics4/Direct3D9.cpp
+++ Backends/Graphics4/Direct3D9/Sources/kinc/backend/graphics4/Direct3D9.cpp
@@ -490,7 +490,7 @@ bool kinc_g4_swap_buffers() {
 void kinc_g4_set_stencil_reference_value(int value) {}
 
 void kinc_g4_set_blend_constant(float r, float g, float b, float a) {
-	device->SetRenderState(D3DRS_BLENDFACTOR, D3DCOLOR_RGBA(r * 255, g * 255, b * 255, a * 255));
+	device->SetRenderState(D3DRS_BLENDFACTOR, D3DCOLOR_RGBA((int)(r * 255), (int)(g * 255), (int)(b * 255), (int)(a * 255)));
 }
 
 void kinc_g4_set_bool(kinc_g4_constant_location_t position, bool value) {
@@ -804,7 +804,7 @@ void kinc_g4_get_features(kinc_g4_features_t *features) {
 	}
 
 	features->blendConstants = (pCaps.SrcBlendCaps & D3DPBLENDCAPS_BLENDFACTOR == D3DPBLENDCAPS_BLENDFACTOR) &&
-	                           (pCaps.DstBlendCaps & D3DPBLENDCAPS_BLENDFACTOR == D3DPBLENDCAPS_BLENDFACTOR);
+	                           (pCaps.DestBlendCaps & D3DPBLENDCAPS_BLENDFACTOR == D3DPBLENDCAPS_BLENDFACTOR);
 }
 
 void kinc_g4_get_limits(kinc_g4_limits_t *limits) {
