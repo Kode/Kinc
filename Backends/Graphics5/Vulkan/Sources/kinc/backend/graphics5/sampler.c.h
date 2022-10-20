24ba2f09f582ac9de092ef0477fd1c3c531855a7
Robert Konrad <git2016@robdangero.us>
2022-10-20 22:15:35 +0200

    Fix the Vulkan-backend

--- Backends/Graphics5/Vulkan/Sources/kinc/backend/graphics5/sampler.c.h
+++ Backends/Graphics5/Vulkan/Sources/kinc/backend/graphics5/sampler.c.h
@@ -3,6 +3,7 @@
 #include <vulkan/vulkan_core.h>
 
 static VkCompareOp convert_compare_mode(kinc_g5_compare_mode_t compare);
+
 static VkSamplerAddressMode convert_addressing(kinc_g5_texture_addressing_t mode) {
 	switch (mode) {
 	case KINC_G5_TEXTURE_ADDRESSING_REPEAT:
@@ -13,6 +14,9 @@ static VkSamplerAddressMode convert_addressing(kinc_g5_texture_addressing_t mode
 		return VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
 	case KINC_G5_TEXTURE_ADDRESSING_MIRROR:
 		return VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT;
+	default:
+		assert(false);
+		return VK_SAMPLER_ADDRESS_MODE_REPEAT;
 	}
 }
 
@@ -23,6 +27,9 @@ static VkSamplerMipmapMode convert_mipmap_mode(kinc_g5_mipmap_filter_t filter) {
 		return VK_SAMPLER_MIPMAP_MODE_NEAREST;
 	case KINC_G5_MIPMAP_FILTER_LINEAR:
 		return VK_SAMPLER_MIPMAP_MODE_LINEAR;
+	default:
+		assert(false);
+		return VK_SAMPLER_MIPMAP_MODE_NEAREST;
 	}
 }
 
@@ -34,33 +41,36 @@ static VkFilter convert_texture_filter(kinc_g5_texture_filter_t filter) {
 		return VK_FILTER_LINEAR;
 	case KINC_G5_TEXTURE_FILTER_ANISOTROPIC:
 		return VK_FILTER_LINEAR; // ?
+	default:
+		assert(false);
+		return VK_FILTER_NEAREST;
 	}
 }
 
-void kinc_g5_sampler_create(kinc_g5_sampler_t *sampler, const kinc_g5_sampler_descriptor_t *descriptor) {
+void kinc_g5_sampler_create(kinc_g5_sampler_t *sampler, const kinc_g5_sampler_options_t *options) {
 	VkSamplerCreateInfo info = {0};
 	info.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
 	info.pNext = NULL;
 	info.flags = 0;
 
-	info.addressModeU = convert_addressing(descriptor->u_addressing);
-	info.addressModeV = convert_addressing(descriptor->v_addressing);
-	info.addressModeW = convert_addressing(descriptor->w_addressing);
+	info.addressModeU = convert_addressing(options->u_addressing);
+	info.addressModeV = convert_addressing(options->v_addressing);
+	info.addressModeW = convert_addressing(options->w_addressing);
 
-	info.mipmapMode = convert_mipmap_mode(descriptor->mipmap_filter);
+	info.mipmapMode = convert_mipmap_mode(options->mipmap_filter);
 
-	info.magFilter = convert_texture_filter(descriptor->magnification_filter);
-	info.minFilter = convert_texture_filter(descriptor->minification_filter);
+	info.magFilter = convert_texture_filter(options->magnification_filter);
+	info.minFilter = convert_texture_filter(options->minification_filter);
 
-	info.compareEnable = descriptor->is_comparison;
-	info.compareOp = convert_compare_mode(descriptor->compare_mode);
+	info.compareEnable = options->is_comparison;
+	info.compareOp = convert_compare_mode(options->compare_mode);
 
 	info.anisotropyEnable =
-	    (descriptor->magnification_filter == KINC_G5_TEXTURE_FILTER_ANISOTROPIC || descriptor->minification_filter == KINC_G5_TEXTURE_FILTER_ANISOTROPIC);
-	info.maxAnisotropy = descriptor->max_anisotropy;
+	    (options->magnification_filter == KINC_G5_TEXTURE_FILTER_ANISOTROPIC || options->minification_filter == KINC_G5_TEXTURE_FILTER_ANISOTROPIC);
+	info.maxAnisotropy = options->max_anisotropy;
 
-	info.maxLod = descriptor->lod_max_clamp;
-	info.minLod = descriptor->lod_min_clamp;
+	info.maxLod = options->lod_max_clamp;
+	info.minLod = options->lod_min_clamp;
 
 	vkCreateSampler(vk_ctx.device, &info, NULL, &sampler->impl.sampler);
 }
