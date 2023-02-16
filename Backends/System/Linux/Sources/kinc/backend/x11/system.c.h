b64a69036aff69062d905a38eb67171852927d51
mundusnine <mundusnine@gmail.com>
2023-02-16 16:43:26 -0500

    Add Windows-consistent behaviour to X11 drop file

--- Backends/System/Linux/Sources/kinc/backend/x11/system.c.h
+++ Backends/System/Linux/Sources/kinc/backend/x11/system.c.h
@@ -781,12 +781,22 @@ bool kinc_x11_handle_messages() {
 				xlib.XGetWindowProperty(x11_ctx.display, event.xselection.requestor, event.xselection.property, 0, LONG_MAX, False, event.xselection.target,
 				                        &type, &format, &numItems, &bytesAfter, &data);
 				size_t len = numItems;
-				while(data[len-1] == '\n' || data[len-1] == '\r'){ data[--len] =0;} // Strip new line at the end
-				wchar_t filePath[len + 1];
-				mbstowcs(filePath, (char *)data, len);
+				size_t pos = 0;
+				char file[260] = {0};
+				size_t f_len = 0;
+				while(pos < len){
+					if(data[pos] == '\r'){//Found a file
+						wchar_t filePath[f_len + 1];
+						mbstowcs(filePath, file, f_len);
+						memset(file,0,f_len);
+						f_len = 0;
+						pos+=2;// Avoid \n
+						filePath[f_len] = 0;
+						kinc_internal_drop_files_callback(filePath + 7); // Strip file://
+					}
+					file[f_len++] = data[pos++];
+				}
 				xlib.XFree(data);
-				filePath[len] = 0;
-				kinc_internal_drop_files_callback(filePath + 7); // Strip file://
 			}
 			break;
 		}
