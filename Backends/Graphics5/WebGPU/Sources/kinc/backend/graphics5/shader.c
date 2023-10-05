f5b9f654246e8f7c8303f74f8fdf6b13b889a9ba
Robert Konrad <git2016@robdangero.us>
2023-10-05 18:42:46 +0200

    [WebGPU] Create one big shader module for Kong

--- Backends/Graphics5/WebGPU/Sources/kinc/backend/graphics5/shader.c
+++ Backends/Graphics5/WebGPU/Sources/kinc/backend/graphics5/shader.c
@@ -4,6 +4,24 @@
 
 extern WGPUDevice device;
 
+#ifdef KINC_KONG
+static WGPUShaderModule shader_module;
+
+void kinc_g5_internal_webgpu_create_shader_module(const void *source, size_t length) {
+	WGPUShaderModuleWGSLDescriptor wgsl_desc = {0};
+	wgsl_desc.code = (const char *)source;
+	wgsl_desc.chain.sType = WGPUSType_ShaderModuleWGSLDescriptor;
+
+	WGPUShaderModuleDescriptor desc = {0};
+	desc.nextInChain = (WGPUChainedStruct *)(&wgsl_desc);
+
+	shader_module = wgpuDeviceCreateShaderModule(device, &desc);
+}
+
+void kinc_g5_shader_init(kinc_g5_shader_t *shader, const void *source, size_t length, kinc_g5_shader_type_t type) {
+	strcpy(&shader->impl.entry_name[0], source);
+}
+#else
 void kinc_g5_shader_init(kinc_g5_shader_t *shader, const void *source, size_t length, kinc_g5_shader_type_t type) {
 	WGPUShaderModuleSPIRVDescriptor smSpirvDesc;
 	memset(&smSpirvDesc, 0, sizeof(smSpirvDesc));
@@ -15,5 +33,6 @@ void kinc_g5_shader_init(kinc_g5_shader_t *shader, const void *source, size_t le
 	smDesc.nextInChain = &smSpirvDesc;
 	shader->impl.module = wgpuDeviceCreateShaderModule(device, &smDesc);
 }
+#endif
 
 void kinc_g5_shader_destroy(kinc_g5_shader_t *shader) {}
