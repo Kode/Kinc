3fa0f4e926d0f204d66f76579b6579328d02fdc9
Robert Konrad <git2016@robdangero.us>
2024-02-04 15:15:07 +0100

    Search for backends in more directories

--- kfile.js
+++ kfile.js
@@ -567,7 +567,16 @@ if (plugin) {
 	else if (platform === Platform.PS5) {
 		backend = 'PlayStation5'
 	}
-	const pluginPath = path.join(__dirname, '..', 'Backends', backend);
+
+	let ancestor = project;
+	while (ancestor.parent != null) {
+		ancestor = ancestor.parent;
+	}
+
+	let pluginPath = path.join(ancestor.basedir, 'Backends', backend);
+	if (!fs.existsSync(pluginPath)) {
+		pluginPath = path.join(__dirname, '..', 'Backends', backend);
+	}
 	if (!fs.existsSync(pluginPath)) {
 		log.error('Was looking for a backend in ' + pluginPath + ' but it wasn\'t there.');
 		throw 'backend not found';
