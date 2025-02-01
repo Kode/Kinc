cb799a1dc9466b5affaf3e516f72cb250df81b7a
Robert Konrad <git2016@robdangero.us>
2025-02-01 22:46:40 +0100

    Add Metal to the API-selection

--- Sources/kope/graphics5/api.h
+++ Sources/kope/graphics5/api.h
@@ -89,9 +89,21 @@ typedef enum kope_g5_api { KOPE_G5_API_DIRECT3D12, KOPE_G5_API_VULKAN } kope_g5_
 
 #endif
 
+#elif defined(KOPE_METAL)
+
+#define KOPE_G5_IMPL(name) kope_metal_##name metal
+#define KOPE_G5_CALL(name) kope_metal_##name()
+#define KOPE_G5_CALL1(name, arg0) kope_metal_##name(arg0)
+#define KOPE_G5_CALL2(name, arg0, arg1) kope_metal_##name(arg0, arg1)
+#define KOPE_G5_CALL3(name, arg0, arg1, arg2) kope_metal_##name(arg0, arg1, arg2)
+#define KOPE_G5_CALL4(name, arg0, arg1, arg2, arg3) kope_metal_##name(arg0, arg1, arg2, arg3)
+#define KOPE_G5_CALL5(name, arg0, arg1, arg2, arg3, arg4) kope_metal_##name(arg0, arg1, arg2, arg3, arg4)
+#define KOPE_G5_CALL6(name, arg0, arg1, arg2, arg3, arg4, arg5) kope_metal_##name(arg0, arg1, arg2, arg3, arg4, arg5)
+#define KOPE_G5_CALL7(name, arg0, arg1, arg2, arg3, arg4, arg5, arg6) kope_metal_##name(arg0, arg1, arg2, arg3, arg4, arg5, arg6)
+
 #elif defined(KOPE_VULKAN)
 
-#define KOPE_G5_IMPL(name) kope_vulkan_##name d3d12
+#define KOPE_G5_IMPL(name) kope_vulkan_##name vulkan
 #define KOPE_G5_CALL(name) kope_vulkan_##name()
 #define KOPE_G5_CALL1(name, arg0) kope_vulkan_##name(arg0)
 #define KOPE_G5_CALL2(name, arg0, arg1) kope_vulkan_##name(arg0, arg1)
